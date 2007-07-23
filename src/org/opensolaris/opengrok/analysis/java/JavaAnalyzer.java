ea84896d2d0347506144dd493af2d1a1a08c1432
Trond Norbye <trond.norbye@sun.com>
2007-07-23 15:37:06 +0200

    Added CDDL header

--- src/org/opensolaris/opengrok/analysis/java/JavaAnalyzer.java
+++ src/org/opensolaris/opengrok/analysis/java/JavaAnalyzer.java
@@ -1,12 +1,26 @@
 /*
- * JavaAnalyzer.java
+ * CDDL HEADER START
  *
- * Created on July 13, 2006, 9:44 PM
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
  *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
+ * See LICENSE.txt included in this distribution for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at LICENSE.txt.
+ * If applicable, add the following below this CDDL HEADER, with the
+ * fields enclosed by brackets "[]" replaced with your own identifying
+ * information: Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
  */
 
+/*
+ * Copyright 2007 Sun Microsystems, Inc.  All rights reserved.
+ * Use is subject to license terms.
+ */
 package org.opensolaris.opengrok.analysis.java;
 
 import org.apache.lucene.document.*;
--- src/org/opensolaris/opengrok/index/IndexerWizard.java
+++ src/org/opensolaris/opengrok/index/IndexerWizard.java
@@ -1,9 +1,26 @@
 /*
- * IndexerWizard.java
+ * CDDL HEADER START
  *
- * Created on January 26, 2006, 10:15 AM
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
+ *
+ * See LICENSE.txt included in this distribution for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at LICENSE.txt.
+ * If applicable, add the following below this CDDL HEADER, with the
+ * fields enclosed by brackets "[]" replaced with your own identifying
+ * information: Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
  */
 
+/*
+ * Copyright 2007 Sun Microsystems, Inc.  All rights reserved.
+ * Use is subject to license terms.
+ */
 package org.opensolaris.opengrok.index;
 
 import java.awt.Dimension;
