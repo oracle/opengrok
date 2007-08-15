cdac985b3e206fe480e2846464d8ce314b36b48b
Trond Norbye <trond.norbye@sun.com>
2007-08-15 09:50:40 +0200

    Replaced TAB character with space

--- src/org/opensolaris/opengrok/history/Annotation.java
+++ src/org/opensolaris/opengrok/history/Annotation.java
@@ -101,7 +101,7 @@ public class Annotation {
      * @param author author name
      */
     void addLine(String revision, String author) {
-	final Line line = new Line(revision, author);
+        final Line line = new Line(revision, author);
         lines.add(line);
         widestRevision = Math.max(widestRevision, line.revision.length());
         widestAuthor = Math.max(widestAuthor, line.author.length());
