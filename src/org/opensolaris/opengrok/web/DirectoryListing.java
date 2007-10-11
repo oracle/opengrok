5d829b1478759d50e47a4e15977e61a4df21c93b
Trond Norbye <trond.norbye@sun.com>
2007-10-11 21:51:29 +0200

    Add JUnit test to verify DirectoryListing

--- src/org/opensolaris/opengrok/web/DirectoryListing.java
+++ src/org/opensolaris/opengrok/web/DirectoryListing.java
@@ -2,7 +2,7 @@
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License (the "License").  
+ * Common Development and Distribution License (the "License").
  * You may not use this file except in compliance with the License.
  *
  * See LICENSE.txt included in this distribution for the specific
@@ -18,20 +18,21 @@
  */
 
 /*
- * Copyright 2005 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2007 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
-
-/*
- * ident	"@(#)DirectoryListing.java 1.2     05/12/01 SMI"
- */
-
 package org.opensolaris.opengrok.web;
 
-import java.io.*;
-import java.util.*;
-import java.text.*;
-import java.util.HashSet;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
 import org.opensolaris.opengrok.configuration.RuntimeEnvironment;
 import org.opensolaris.opengrok.index.IgnoredNames;
 
@@ -39,27 +40,27 @@ import org.opensolaris.opengrok.index.IgnoredNames;
  * Generates HTML listing of a Directory
  */
 public class DirectoryListing {
+
     private EftarFileReader desc;
     private long now;
-    
+
     public DirectoryListing() {
-	desc = null;
-	now =  (new Date()).getTime();
+        desc = null;
+        now = System.currentTimeMillis();
     }
-    
+
     public DirectoryListing(EftarFileReader desc) {
-	this.desc = desc;
-	now =  (new Date()).getTime();
+        this.desc = desc;
+        now = System.currentTimeMillis();
     }
-    
+
     public void listTo(File dir, Writer out) throws IOException {
-	String[] files = dir.list();
-	if (files != null) {
-	    Arrays.sort(files, String.CASE_INSENSITIVE_ORDER);
-	    listTo(dir, out, dir.getPath(), files);
-	}
+        String[] files = dir.list();
+        if (files != null) {
+            listTo(dir, out, dir.getPath(), files);
+        }
     }
-    
+
     /**
      * Write a listing of "dir" to "out"
      *
@@ -67,100 +68,79 @@ public class DirectoryListing {
      * @param out writer to write
      * @param path Virtual Path of the directory
      * @param files childred of dir
+     * @return a list of READMEs
      * @throws java.io.IOException
-     * Returns a list of READMEs
      *
      */
     public ArrayList listTo(File dir, Writer out, String path, String[] files) throws IOException {
-	Arrays.sort(files, String.CASE_INSENSITIVE_ORDER);
-	boolean alt = true;
-	NumberFormat formatter = new DecimalFormat("#,###,###,###");
-	Format dateFormatter = new SimpleDateFormat("dd-MMM-yyyy");
-	out.write("<table cellspacing=\"0\" border=\"0\" id=\"dirlist\">");
-	EftarFileReader.FNode parentFNode = null;
-	if (!path.equals("")) {
-	    out.write("<tr><td colspan=\"4\"><a href=\"..\"><i>Up to higher level directory</i></a></td></tr>");
-	}
-	if(desc != null)
-	    parentFNode =  desc.getNode(path);
-	out.write("<tr class=\"thead\"><th><tt>Name</tt></th><th><tt>Date</tt></th><th><tt>Size</tt></th>");
-/*        if (fstats != null) {
-	    out.write("<th><tt>Lines</tt></th><th><tt>Files</tt></th>");
-	}
- */
-	if(parentFNode!= null && parentFNode.childOffset > 0) {
-	    out.write("<th><tt>Description</tt></th>");
-	}
-	out.write("</tr>");
-	ArrayList<String> readMes = new ArrayList<String>();
+        Arrays.sort(files, String.CASE_INSENSITIVE_ORDER);
+        boolean alt = true;
+        Format dateFormatter = new SimpleDateFormat("dd-MMM-yyyy");
+        out.write("<table cellspacing=\"0\" border=\"0\" id=\"dirlist\">");
+        EftarFileReader.FNode parentFNode = null;
+        if (!path.equals("")) {
+            out.write("<tr><td colspan=\"4\"><a href=\"..\"><i>Up to higher level directory</i></a></td></tr>");
+        }
+        if (desc != null) {
+            parentFNode = desc.getNode(path);
+        }
+        out.write("<tr class=\"thead\"><th><tt>Name</tt></th><th><tt>Date</tt></th><th><tt>Size</tt></th>");
+
+        if (parentFNode != null && parentFNode.childOffset > 0) {
+            out.write("<th><tt>Description</tt></th>");
+        }
+        out.write("</tr>");
+        ArrayList<String> readMes = new ArrayList<String>();
         IgnoredNames ignoredNames = RuntimeEnvironment.getInstance().getIgnoredNames();
-	for (int i = 0; i < files.length; i++) {
-	    if(!ignoredNames.ignore(files[i])) {
-		File child = new File(dir, files[i]);
-		String count = "";
-		String size = null;
-		String lines = "-";
-		if (files[i].startsWith("README") || files[i].endsWith("README") || files[i].startsWith("readme")){
-		    readMes.add(files[i]);
-		}
-		alt ^= true;
-		out.write("<tr align=\"right\"");
-		if(alt)
-		    out.write(" class=\"alt\"");
-		
-		boolean isDir = child.isDirectory();
-		out.write("><td align=\"left\"><tt> <a href=\"" + files[i] + (isDir ? "/\" class=\"r\"" : "\" class=\"p\"") + ">");
-		if(isDir) {
-		    out.write("<b>" + files[i] + "</b></a>/");
-		} else {
-		    out.write(        files[i] +     "</a>");
-		}
-		Date lastm = new Date(child.lastModified());
-		out.write("</tt></td><td>" + ((now - lastm.getTime()) < 86400000 ? "Today" : dateFormatter.format(lastm))
-		+ "</td>");
-/*                if (fstats != null) {
-		    SizeandLines snl = fstats.get(path + "/" +files[i]);
-		    //System.out.println("Fstats for = " + path + "/" +files[i] + " = " + snl);
-		    if(snl != null) {
-			out.write("<td><tt>" + Util.redableSize(snl.size) + "</tt></td>");
-			out.write("<td><tt>" + formatter.format(snl.lines) + "</tt></td>");
-			out.write("<td><tt>" + (isDir ? formatter.format(snl.count) : "") + "</tt></td>");
-		    } else {
-			out.write("<td>-</td><td>-</td><td>-</td>");
- 
-		    }
-		} else
- */
-		out.write("<td><tt>" + (isDir ? "" : Util.redableSize(child.length())) + "</tt></td>");
-		
-		if(parentFNode != null  && parentFNode.childOffset > 0) {
-		    String briefDesc = desc.getChildTag(parentFNode, files[i]);
-		    if (briefDesc != null) {
-			out.write("<td align=\"left\">");
-			out.write(briefDesc);
-			out.write("</td>");
-		    } else {
-			out.write("<td></td>");
-		    }
-		}
-		out.write("</tr>");
-	    }
-	}
-	out.write("</table>");
-	return (readMes);
+
+        for (String file : files) {
+            if (!ignoredNames.ignore(file)) {
+                File child = new File(dir, file);
+                if (file.startsWith("README") || file.endsWith("README") || file.startsWith("readme")) {
+                    readMes.add(file);
+                }
+                alt = !alt;
+                out.write("<tr align=\"right\"");
+                out.write(alt ? " class=\"alt\"" : "");
+
+                boolean isDir = child.isDirectory();
+                out.write("><td align=\"left\"><tt><a href=\"" + file + (isDir ? "/\" class=\"r\"" : "\" class=\"p\"") + ">");
+                if (isDir) {
+                    out.write("<b>" + file + "</b></a>/");
+                } else {
+                    out.write(file + "</a>");
+                }
+                Date lastm = new Date(child.lastModified());
+                out.write("</tt></td><td>" + ((now - lastm.getTime()) < 86400000 ? "Today" : dateFormatter.format(lastm))		+ "</td>");
+                out.write("<td><tt>" + (isDir ? "" : Util.redableSize(child.length())) + "</tt></td>");
+
+                if (parentFNode != null && parentFNode.childOffset > 0) {
+                    String briefDesc = desc.getChildTag(parentFNode, file);
+                    if (briefDesc != null) {
+                        out.write("<td align=\"left\">");
+                        out.write(briefDesc);
+                        out.write("</td>");
+                    } else {
+                        out.write("<td></td>");
+                    }
+                }
+                out.write("</tr>");
+            }
+        }
+        out.write("</table>");
+        return readMes;
     }
-    
-    public static void main(String args[]) { try {
-	DirectoryListing dl = new DirectoryListing();
-	File tolist = new File(args[0]);
-	File outFile = new File(args[1]);
-	BufferedWriter out = new BufferedWriter(new FileWriter(outFile));
-	//Page.mast(args[0], out, true);
-	dl.listTo(tolist, out);
-	//Page.footer(out);
-	out.close();
-    } catch ( Exception e) {
-	System.out.println(" ERROR " + e + "\n Usage DirListing <dir> <output.html>");
-    }
+
+    public static void main(String[] args) {
+        try {
+            DirectoryListing dl = new DirectoryListing();
+            File tolist = new File(args[0]);
+            File outFile = new File(args[1]);
+            BufferedWriter out = new BufferedWriter(new FileWriter(outFile));
+            dl.listTo(tolist, out);
+            out.close();
+        } catch (Exception e) {
+            System.out.println(" ERROR " + e + "\n Usage DirListing <dir> <output.html>");
+        }
     }
 }
---
+++ test/org/opensolaris/opengrok/web/DirectoryListingTest.java
@@ -0,0 +1,272 @@
+/*
+ * CDDL HEADER START
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
+ *
+ * See LICENSE.txt included in this distribution for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at LICENSE.txt.
+ * If applicable, add the following below this CDDL HEADER, with the
+ * fields enclosed by brackets "[]" replaced with your own identifying
+ * information: Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ */
+
+/*
+ * Copyright 2007 Sun Microsystems, Inc.  All rights reserved.
+ * Use is subject to license terms.
+ */
+package org.opensolaris.opengrok.web;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.StringWriter;
+import java.text.SimpleDateFormat;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+/**
+ * JUnit test to test that the DirectoryListing produce the expected result
+ */
+public class DirectoryListingTest {
+
+    private File directory;
+    private FileEntry[] entries;
+    private SimpleDateFormat dateFormatter;
+
+    class FileEntry implements Comparable {
+        FileEntry() {
+            dateFormatter = new SimpleDateFormat("dd-MMM-yyyy");
+        }
+
+        FileEntry(String name, long lastModified, int size) {
+            this.name = name;
+            this.lastModified = lastModified;
+            this.size = size;
+        }
+
+        public void create() throws Exception {
+            File file = new File(directory, name);
+            if (!file.exists() && !file.createNewFile()) {
+                throw new Exception("Failed to create file");
+            }
+
+            long val = lastModified;
+            if (val == Long.MAX_VALUE) {
+                val = System.currentTimeMillis();
+            }
+
+            if (!file.setLastModified(val)) {
+                throw new Exception("Failed to set modification time");
+            }
+
+            if (size > 0) {
+                FileOutputStream out = new FileOutputStream(file);
+                byte[] buffer = new byte[size];
+                out.write(buffer);
+                out.close();
+            }
+        }
+        String name;
+        long lastModified;
+        int size;
+
+        public int compareTo(Object o) {
+            if (o instanceof FileEntry) {
+                FileEntry fe = (FileEntry) o;
+
+                // @todo verify all attributes!
+                return name.compareTo(fe.name);
+            } else {
+                return -1;
+            }
+        }
+    }
+
+    public DirectoryListingTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        directory = File.createTempFile("directory", "");
+        if (directory.exists() && !directory.delete()) {
+            throw new Exception("Failed to remove test-directory");
+        }
+        if (!directory.mkdir()) {
+            throw new Exception("Failed to create test-directory");
+        }
+
+        entries = new FileEntry[2];
+        entries[0] = new FileEntry("foo", 0, 0);
+        entries[1] = new FileEntry("bar", Long.MAX_VALUE, 0);
+
+        for (FileEntry entry : entries) {
+            entry.create();
+        }
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        if (directory != null && directory.exists()) {
+            removeDirectory(directory);
+        }
+    }
+
+    private void removeDirectory(File dir) {
+        File[] childs = dir.listFiles();
+        if (childs != null) {
+            for (File f : childs) {
+
+                if (f.isDirectory()) {
+                    removeDirectory(f);
+                }
+                f.delete();
+            }
+        }
+    }
+
+    /**
+     * Get the filename from: &lt;td align="left"&gt;&lt;tt&gt;&lt;a href="foo"
+     * class="p"&gt;foo&lt;/a&gt;&lt;/tt&gt;&lt;/td&gt;
+     *
+     * @param item
+     * @return
+     * @throws java.lang.Exception
+     */
+    private String getFilename(Node item) throws Exception {
+        Node node = item.getFirstChild(); // tt
+        assertNotNull(node);
+        assertEquals(Node.ELEMENT_NODE, node.getNodeType());
+        node = node.getFirstChild(); // a
+        assertNotNull(node);
+        assertEquals(Node.ELEMENT_NODE, node.getNodeType());
+        node = node.getFirstChild();
+        assertNotNull(node);
+        assertEquals(Node.TEXT_NODE, node.getNodeType());
+        return node.getNodeValue();
+    }
+
+    /**
+     * Get the LastModified date from the &lt;td&gt;date&lt;/td&gt;
+     * @todo fix the item
+     * @param item the node representing &lt;td&gt
+     * @return last modified date of the file
+     * @throws java.lang.Exception if an error occurs
+     */
+    private long getLastModified(Node item) throws Exception {
+        Node val = item.getFirstChild();
+        assertNotNull(val);
+        assertEquals(Node.TEXT_NODE, val.getNodeType());
+
+        String value = val.getNodeValue();
+        if (value.equalsIgnoreCase("Today")) {
+            return Long.MAX_VALUE;
+        } else {
+            return dateFormatter.parse(value).getTime();
+        }
+    }
+
+    /**
+     * Get the size from the: &lt;td&gt;&lt;tt&gt;size&lt;/tt&gt;&lt;/td&gt;
+     * @param item the node representing &lt;td&gt;
+     * @return The size
+     * @throws java.lang.Exception if an error occurs
+     */
+    private int getSize(Node item) throws Exception {
+        Node child = item.getFirstChild();
+        assertNotNull(child);
+        Node val = child.getFirstChild();
+        assertNotNull(val);
+        assertEquals(Node.TEXT_NODE, val.getNodeType());
+        return Integer.parseInt(val.getNodeValue());
+    }
+
+    /**
+     * Validate this file-entry in the table
+     * @param element The &lt;tr&gt; element
+     * @throws java.lang.Exception
+     */
+    private void validateEntry(Element element) throws Exception {
+        FileEntry entry = new FileEntry();
+        NodeList nl = element.getElementsByTagName("td");
+        int len = nl.getLength();
+        if (len < 3) {
+            return;
+        }
+        assertEquals(3, len);
+
+        entry.name = getFilename(nl.item(0));
+        entry.lastModified = getLastModified(nl.item(1));
+        entry.size = getSize(nl.item(2));
+
+        // Try to look it up in the list of files
+        for (int ii = 0; ii < entries.length; ++ii) {
+            if (entries[ii] != null && entries[ii].compareTo(entry) == 0) {
+                entries[ii] = null;
+                return;
+            }
+        }
+
+        fail("Could not find a match for: " + entry.name);
+    }
+
+    /**
+     * Test directory listing
+     * @throws java.lang.Exception if an error occurs while generating the
+     *         list.
+     */
+    @Test
+    public void directoryListing() throws Exception {
+        StringWriter out = new StringWriter();
+        out.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
+
+        DirectoryListing instance = new DirectoryListing();
+        instance.listTo(directory, out);
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        if (factory == null) {
+            throw new Exception("Internal error. Failed to create / initialize XML parser (DocumentBuilderFactory)");
+        }
+
+        DocumentBuilder builder = factory.newDocumentBuilder();
+
+        if (builder == null) {
+            throw new Exception("Internal error. Failed to create / initialize XML parser (DocumentBuilder)");
+        }
+
+        String str = out.toString();
+        Document document = builder.parse(new ByteArrayInputStream(str.getBytes()));
+
+        NodeList nl = document.getElementsByTagName("tr");
+        int len = nl.getLength();
+        assertEquals(entries.length + 2, len);
+        // Skip the .. entry and the header
+        for (int i = 2; i < len; ++i) {
+            validateEntry((Element) nl.item(i));
+        }
+    }
+}
