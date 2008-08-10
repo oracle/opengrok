4e686352d9850087844f97f385dc4a217471e8b6
Trond Norbye <trond.norbye@sun.com>
2008-08-10 12:24:23 +0200

    Check return value for mkdirs (findbugs)

--- src/org/opensolaris/opengrok/OpenGrokLogger.java
+++ src/org/opensolaris/opengrok/OpenGrokLogger.java
@@ -21,7 +21,6 @@
  * Copyright 2008 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
-
 package org.opensolaris.opengrok;
 
 import java.io.File;
@@ -39,66 +38,61 @@ import java.util.logging.Logger;
  * @author Jan S Berg
  */
 public class OpenGrokLogger {
-   
-   private static int LOGFILESIZELIMIT = 1000000;
-   private static int LOGFILESCOUNT = 30;
-   private final static Logger log = Logger.getLogger("org.opensolaris.opengrok");
-      
-   public static String setupLogger(String logpath, Level filelevel, Level consolelevel) {
-            
-      //defaults to tmp
-      if (logpath==null) {
-         logpath = "%t";
-      }
-      System.out.println("Logging to " + logpath);
-      
-      try {
-         File jlp = new File(logpath);
-         jlp.mkdirs();
-      }
-      catch (Exception ex2) {
-         log.severe("could not make logpath: " + ex2.getMessage());
-      }
 
-      clearForeignHandlers();
-      String logfile = logpath + File.separatorChar + "opengrok%g.%u" + ".log";
-      try {
+    private static int LOGFILESIZELIMIT = 1000000;
+    private static int LOGFILESCOUNT = 30;
+    private final static Logger log = Logger.getLogger("org.opensolaris.opengrok");
+
+    public static String setupLogger(String logpath, Level filelevel, Level consolelevel) {
+        if (logpath == null) {
+            logpath = "%t";
+        }
+        System.out.println("Logging to " + logpath);
+        File jlp = new File(logpath);
+        if (!jlp.exists() && !jlp.mkdirs()) {
+            throw new RuntimeException("could not make logpath: " +
+                    jlp.getAbsolutePath());
+        }
+
+        clearForeignHandlers();
+        String logfile = logpath + File.separatorChar + "opengrok%g.%u" + ".log";
+        try {
             FileHandler fh = new FileHandler(logfile,
-            LOGFILESIZELIMIT, // size (unlimited)
-            LOGFILESCOUNT); // # rotations
-            
+                    LOGFILESIZELIMIT, // size (unlimited)
+                    LOGFILESCOUNT); // # rotations
+
             fh.setLevel(filelevel);
             fh.setFormatter(new FileLogFormatter());
-            
-         log.addHandler(fh);
-         
-         ConsoleHandler ch = new ConsoleHandler();
-         ch.setLevel(consolelevel);
-         ch.setFormatter(new ConsoleFormatter());
-         log.addHandler(ch);
-         
-      }
-      catch (Exception ex1) {
-         System.err.println("Exception logging " + ex1);
-      }
-      log.setLevel(filelevel);
-      return logpath;
-   }
-   
-   private static void clearForeignHandlers()
-   {
-      for (Enumeration e = LogManager.getLogManager().getLoggerNames();
-           e.hasMoreElements(); ) {
-         String loggerName = (String)e.nextElement();
-         Logger l = Logger.getLogger(loggerName);
-         Handler[] h = l.getHandlers();
-         if (!loggerName.startsWith("org.opensolaris.opengrok")) {
-            for (int i=0; i<h.length; ++i) {
-               l.removeHandler(h[i]);
+
+            log.addHandler(fh);
+
+            ConsoleHandler ch = new ConsoleHandler();
+            ch.setLevel(consolelevel);
+            ch.setFormatter(new ConsoleFormatter());
+            log.addHandler(ch);
+
+        } catch (Exception ex1) {
+            System.err.println("Exception logging " + ex1);
+        }
+        log.setLevel(filelevel);
+        return logpath;
+    }
+
+    private static void clearForeignHandlers() {
+        for (Enumeration e = LogManager.getLogManager().getLoggerNames();
+                e.hasMoreElements();) {
+            String loggerName = (String) e.nextElement();
+            Logger l = Logger.getLogger(loggerName);
+            Handler[] h = l.getHandlers();
+            if (!loggerName.startsWith("org.opensolaris.opengrok")) {
+                for (int i = 0; i < h.length; ++i) {
+                    l.removeHandler(h[i]);
+                }
             }
-         }
-         h = l.getHandlers();
-      }
-   }
-   
+            h = l.getHandlers();
+        }
+    }
+
+    private OpenGrokLogger() {
+    }
 }
