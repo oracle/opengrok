977ca7219c3d0d9aaa8e04f68c350351654b20c1
Trond Norbye <trond.norbye@sun.com>
2008-08-10 13:54:52 +0200

    Added a new test for Indexer.main

--- test/org/opensolaris/opengrok/index/IndexerTest.java
+++ test/org/opensolaris/opengrok/index/IndexerTest.java
@@ -113,7 +113,7 @@ public class IndexerTest {
      */
     @Test
     public void testIndexGeneration() throws Exception {
-        System.out.println("Generating index");
+        System.out.println("Generating index by using the class methods");
         RuntimeEnvironment env = RuntimeEnvironment.getInstance();
         env.setCtags(System.getProperty("org.opensolaris.opengrok.configuration.ctags", "ctags"));
         if (env.validateExuberantCtags()) {
@@ -122,6 +122,22 @@ public class IndexerTest {
             env.setVerbose(true);
             Indexer.getInstance().prepareIndexer(env, true, true, "/c", null, false, false, false, null, null);
             Indexer.getInstance().doIndexerExecution(true, 1, null, null);
+        } else {
+            System.out.println("Skipping test. Could not find a ctags I could use in path.");
+        }
+    }
+
+    /**
+     * Test of doIndexerExecution method, of class Indexer.
+     */
+    @Test
+    public void testMain() throws IOException {
+        System.out.println("Generate index by using command line options");
+        RuntimeEnvironment env = RuntimeEnvironment.getInstance();
+        env.setCtags(System.getProperty("org.opensolaris.opengrok.configuration.ctags", "ctags"));
+        if (env.validateExuberantCtags()) {
+            String[] argv = { "-S", "-P", "-p", "/c", "-H", "-Q", "off", "-s", sourceRoot.getAbsolutePath(), "-d", dataRoot.getAbsolutePath(), "-v"};
+            Indexer.main(argv);
         } else {
             System.out.println("Skipping test. Could not find a ctags I could use in path.");
         }
