f2ef05243bb7e143c40a57bdfc646fb4eb854e94
Trond Norbye <trond.norbye@sun.com>
2008-09-03 22:12:35 +0200

    Added a jar-file to the test-data and minor cleanup in JavaClassAnalyzer

--- src/org/opensolaris/opengrok/analysis/executables/JavaClassAnalyzer.java
+++ src/org/opensolaris/opengrok/analysis/executables/JavaClassAnalyzer.java
@@ -65,7 +65,6 @@ import org.opensolaris.opengrok.analysis.TagFilter;
 import org.opensolaris.opengrok.analysis.plain.PlainFullTokenizer;
 import org.opensolaris.opengrok.configuration.RuntimeEnvironment;
 
-
 /**
  * Ananlyzes Java Class files
  * Created on September 23, 2005
@@ -73,13 +72,15 @@ import org.opensolaris.opengrok.configuration.RuntimeEnvironment;
  * @author Chandan
  */
 public class JavaClassAnalyzer extends FileAnalyzer {
+
     private final String urlPrefix = RuntimeEnvironment.getInstance().getUrlPrefix();
-   
-    /** Creates a new instance of JavaClassAnalyzer */
+
+    /** Creates a new instance of JavaClassAnalyzer
+     * @param factory The factory that creates JavaClassAnalyzers
+     */
     protected JavaClassAnalyzer(FileAnalyzerFactory factory) {
         super(factory);
     }
-    
     private List<String> defs;
     private List<String> refs;
     private List<String> full;
@@ -93,15 +94,15 @@ public class JavaClassAnalyzer extends FileAnalyzer {
         defs = new LinkedList<String>();
         refs = new LinkedList<String>();
         full = new LinkedList<String>();
-	fullText = null;
-	xref = null;
+        fullText = null;
+        xref = null;
         try {
-            ClassParser cp = new ClassParser(in,doc.get("path"));
-            c = cp.parse();
+            ClassParser classparser = new ClassParser(in, doc.get("path"));
+            c = classparser.parse();
             StringWriter out = new StringWriter();
             getContent(out);
             xref = out.toString();
-            for(String fl: full) {
+            for (String fl : full) {
                 out.write(fl);
                 out.write('\n');
             }
@@ -113,7 +114,7 @@ public class JavaClassAnalyzer extends FileAnalyzer {
         } catch (org.apache.bcel.classfile.ClassFormatException e) {
             OpenGrokLogger.getLogger().log(Level.WARNING, "An error occured while analyzing stream.", e);
         }
-        if(fullText != null && fullText.length() > 0) {
+        if (fullText != null && fullText.length() > 0) {
             doc.add(new Field("defs", dummy));
             doc.add(new Field("refs", dummy));
             doc.add(new Field("full", dummy));
@@ -123,80 +124,85 @@ public class JavaClassAnalyzer extends FileAnalyzer {
     public List<String> getDefs() {
         return defs;
     }
+
     public List<String> getRefs() {
         return refs;
     }
+
     public String getFull() {
         return fullText;
     }
-    
-    private int[] v ;
+    private int[] v;
     private ConstantPool cp;
-    
+
+    @Override
     public TokenStream tokenStream(String fieldName, Reader reader) {
-        if("defs".equals(fieldName)) {
+        if ("defs".equals(fieldName)) {
             return new List2TokenStream(defs);
-        } else if ( "refs".equals(fieldName)) {
+        } else if ("refs".equals(fieldName)) {
             return new List2TokenStream(refs);
-        } else if ( "full".equals(fieldName)) {
+        } else if ("full".equals(fieldName)) {
             return new PlainFullTokenizer(new TagFilter(new StringReader(fullText)));
         }
         return super.tokenStream(fieldName, reader);
     }
-    
-    public String linkPath(String path) {
+
+    protected String linkPath(String path) {
         return "<a href=\"" + urlPrefix + "path=" + path + "\">" + path + "</a>";
     }
-    
-    public String linkDef(String def) {
+
+    protected String linkDef(String def) {
         return "<a href=\"" + urlPrefix + "defs=" + def + "\">" + def + "</a>";
     }
-    
-    public String tagDef(String def) {
+
+    protected String tagDef(String def) {
         return "<a class=\"d\" name=\"" + def + "\" href=\"" + urlPrefix + "defs=" + def + "\">" + def + "</a>";
     }
-    
-    public void getContent(Writer out) throws IOException {
+
+    private void getContent(Writer out) throws IOException {
         String t;
         cp = c.getConstantPool();
-        v = new int[cp.getLength()+1];
+        v = new int[cp.getLength() + 1];
         out.write(linkPath(t = c.getSourceFileName()));
-        defs.add(t); refs.add(t);
+        defs.add(t);
+        refs.add(t);
         out.write('\n');
-        
+
         out.write("package ");
         out.write(linkDef(t = c.getPackageName()));
-        defs.add(t); refs.add(t);
-        
+        defs.add(t);
+        refs.add(t);
+
         out.write('\n');
         String aflg = null;
         out.write(aflg = Utility.accessToString(c.getAccessFlags(), true));
-        if(aflg != null) {
+        if (aflg != null) {
             out.write(' ');
         }
-        
+
         v[c.getClassNameIndex()] = 1;
         out.write(tagDef(t = c.getClassName()));
-        defs.add(t); refs.add(t);
+        defs.add(t);
+        refs.add(t);
         out.write(" extends ");
-        
+
         v[c.getSuperclassNameIndex()] = 1;
         out.write(linkDef(t = c.getSuperclassName()));
         refs.add(t);
-        for(int i: c.getInterfaceIndices()) {
-            v[i]=1;
+        for (int i : c.getInterfaceIndices()) {
+            v[i] = 1;
         }
         String ins[] = c.getInterfaceNames();
         if (ins != null && ins.length > 0) {
             out.write(" implements ");
-            for(String in: ins) {
+            for (String in : ins) {
                 out.write(linkDef(t = in));
                 refs.add(t);
                 out.write(' ');
             }
         }
         out.write(" {\n");
-        
+
         for (Attribute a : c.getAttributes()) {
             if (a.getTag() == org.apache.bcel.Constants.ATTR_CODE) {
                 for (Attribute ca : ((Code) a).getAttributes()) {
@@ -211,86 +217,88 @@ public class JavaClassAnalyzer extends FileAnalyzer {
                 break;
             }
         }
-        
-        for(org.apache.bcel.classfile.Field fld: c.getFields()) {
+
+        for (org.apache.bcel.classfile.Field fld : c.getFields()) {
             out.write('\t');
             String aflgs;
             out.write(aflgs = Utility.accessToString(fld.getAccessFlags()));
-            if(aflgs != null && aflgs.length() > 0) {
+            if (aflgs != null && aflgs.length() > 0) {
                 out.write(' ');
             }
             out.write(Utility.signatureToString(fld.getSignature()));
             out.write(' ');
             out.write(tagDef(t = fld.getName()));
-            defs.add(t); refs.add(t);
+            defs.add(t);
+            refs.add(t);
             out.write('\n');
-            // @TODO show Attributes
+        // @TODO show Attributes
         }
-        
-        for(org.apache.bcel.classfile.Method m: c.getMethods()) {
+
+        for (org.apache.bcel.classfile.Method m : c.getMethods()) {
             out.write('\t');
             String aflgs;
             out.write(aflgs = Utility.accessToString(m.getAccessFlags()));
-            if(aflgs != null && aflgs.length() > 0) {
+            if (aflgs != null && aflgs.length() > 0) {
                 out.write(' ');
             }
             String sig = m.getSignature();
             out.write(Utility.methodSignatureReturnType(sig, false));
             out.write(' ');
             out.write(tagDef(t = m.getName()));
-            defs.add(t); refs.add(t);
+            defs.add(t);
+            refs.add(t);
             out.write(" (");
-            String [] args = Utility.methodSignatureArgumentTypes(sig, false);
-            for(int i = 0 ; i < args.length; i++) {
+            String[] args = Utility.methodSignatureArgumentTypes(sig, false);
+            for (int i = 0; i < args.length; i++) {
                 out.write(t = args[i]);
                 int spi = t.indexOf(' ');
-                if(spi > 0) {
+                if (spi > 0) {
                     refs.add(t.substring(0, spi));
-                    defs.add(t.substring(spi+1));
+                    defs.add(t.substring(spi + 1));
                 }
-                if(i < args.length - 1) {
+                if (i < args.length - 1) {
                     out.write(", ");
                 }
             }
             out.write(") ");
             ArrayList<LocalVariable[]> locals = new ArrayList<LocalVariable[]>();
-            for(Attribute a: m.getAttributes()) {
-                if(a.getTag() == org.apache.bcel.Constants.ATTR_EXCEPTIONS) {
-                    for(int i: ((ExceptionTable)a).getExceptionIndexTable()) {
+            for (Attribute a : m.getAttributes()) {
+                if (a.getTag() == org.apache.bcel.Constants.ATTR_EXCEPTIONS) {
+                    for (int i : ((ExceptionTable) a).getExceptionIndexTable()) {
                         v[i] = 1;
                     }
-                    String[] exs = ((ExceptionTable)a).getExceptionNames();
-                    if(exs != null && exs.length > 0) {
+                    String[] exs = ((ExceptionTable) a).getExceptionNames();
+                    if (exs != null && exs.length > 0) {
                         out.write(" throws ");
-                        for(String ex: exs) {
+                        for (String ex : exs) {
                             out.write(linkDef(ex));
                             refs.add(ex);
                             out.write(' ');
                         }
                     }
-                } else if(a.getTag() == org.apache.bcel.Constants.ATTR_CODE) {
-                    for(Attribute ca: ((Code)a).getAttributes()) {
-                        if(ca.getTag() == org.apache.bcel.Constants.ATTR_LOCAL_VARIABLE_TABLE) {
-                            locals.add(((LocalVariableTable)ca).getLocalVariableTable());
+                } else if (a.getTag() == org.apache.bcel.Constants.ATTR_CODE) {
+                    for (Attribute ca : ((Code) a).getAttributes()) {
+                        if (ca.getTag() == org.apache.bcel.Constants.ATTR_LOCAL_VARIABLE_TABLE) {
+                            locals.add(((LocalVariableTable) ca).getLocalVariableTable());
                         }
                     }
                 }
             }
             out.write("\n");
             if (!locals.isEmpty()) {
-                for(LocalVariable[] ls: locals) {
-                    for(LocalVariable l: ls) {
+                for (LocalVariable[] ls : locals) {
+                    for (LocalVariable l : ls) {
                         printLocal(out, l);
                     }
                 }
             }
         }
         out.write("}\n");
-        for(int i = 0; i < v.length-1; i++) {
+        for (int i = 0; i < v.length - 1; i++) {
             if (v[i] != 1) {
-                Constant c = cp.getConstant(i);
-                if(c != null) {
-                    full.add(constantToString(c));
+                Constant constant = cp.getConstant(i);
+                if (constant != null) {
+                    full.add(constantToString(constant));
                 }
             }
         }
@@ -300,17 +308,18 @@ public class JavaClassAnalyzer extends FileAnalyzer {
      * Write a cross referenced HTML file.
      * @param out Writer to write HTML cross-reference
      */
+    @Override
     public void writeXref(Writer out) throws IOException {
-    	if(xref != null) {
+        if (xref != null) {
             out.write(xref);
         }
     }
-    
+
     private void printLocal(Writer out, LocalVariable l) throws IOException {
         v[l.getIndex()] = 1;
         v[l.getNameIndex()] = 1;
         v[l.getSignatureIndex()] = 1;
-        if(!"this".equals(l.getName())) {
+        if (!"this".equals(l.getName())) {
             out.write("\t\t");
             out.write(Utility.signatureToString(l.getSignature()));
             out.write(' ');
@@ -321,57 +330,68 @@ public class JavaClassAnalyzer extends FileAnalyzer {
             out.write('\n');
         }
     }
-    
-    public String constantToString(Constant c)  throws ClassFormatException {
-        String   str;
-        int	   i, j;
-        byte     tag = c.getTag();
-        
-        switch(tag) {
+
+    public String constantToString(Constant c) throws ClassFormatException {
+        String str;
+        int i, j;
+        byte tag = c.getTag();
+
+        switch (tag) {
             case org.apache.bcel.Constants.CONSTANT_Class:
-                i	= ((ConstantClass) c).getNameIndex();
-                v[i]=1;
+                i = ((ConstantClass) c).getNameIndex();
+                v[i] = 1;
                 Constant con = cp.getConstant(i, org.apache.bcel.Constants.CONSTANT_Utf8);
                 str = Utility.compactClassName(((ConstantUtf8) con).getBytes(), false);
                 break;
-                
+
             case org.apache.bcel.Constants.CONSTANT_String:
-                i	= ((ConstantString)c).getStringIndex();
-                v[i]=1;
+                i = ((ConstantString) c).getStringIndex();
+                v[i] = 1;
                 Constant con2 = cp.getConstant(i, org.apache.bcel.Constants.CONSTANT_Utf8);
-                str =  ((ConstantUtf8) con2).getBytes();
+                str = ((ConstantUtf8) con2).getBytes();
                 break;
-                
-            case org.apache.bcel.Constants.CONSTANT_Utf8:    str = ((ConstantUtf8)c).toString();         break;
-            case org.apache.bcel.Constants.CONSTANT_Double:  str = ((ConstantDouble)c).toString();  break;
-            case org.apache.bcel.Constants.CONSTANT_Float:   str = ((ConstantFloat)c).toString();   break;
-            case org.apache.bcel.Constants.CONSTANT_Long:    str = ((ConstantLong)c).toString();    break;
-            case org.apache.bcel.Constants.CONSTANT_Integer: str = ((ConstantInteger)c).toString(); break;
-            
+
+            case org.apache.bcel.Constants.CONSTANT_Utf8:
+                str = ((ConstantUtf8) c).toString();
+                break;
+            case org.apache.bcel.Constants.CONSTANT_Double:
+                str = ((ConstantDouble) c).toString();
+                break;
+            case org.apache.bcel.Constants.CONSTANT_Float:
+                str = ((ConstantFloat) c).toString();
+                break;
+            case org.apache.bcel.Constants.CONSTANT_Long:
+                str = ((ConstantLong) c).toString();
+                break;
+            case org.apache.bcel.Constants.CONSTANT_Integer:
+                str = ((ConstantInteger) c).toString();
+                break;
+
             case org.apache.bcel.Constants.CONSTANT_NameAndType:
-                i = ((ConstantNameAndType)c).getNameIndex();
-                v[i]=1;
-                j = ((ConstantNameAndType)c).getSignatureIndex();
-                v[j]=1;
+                i = ((ConstantNameAndType) c).getNameIndex();
+                v[i] = 1;
+                j = ((ConstantNameAndType) c).getSignatureIndex();
+                v[j] = 1;
                 String sig = constantToString(cp.getConstant(j));
-                if(sig.charAt(0) == '(') {
+                if (sig.charAt(0) == '(') {
                     str = Utility.methodSignatureToString(sig, constantToString(cp.getConstant(i)), " ");
                 } else {
                     str = Utility.signatureToString(sig) + ' ' + constantToString(cp.getConstant(i));
                 }
                 //str = constantToString(cp.getConstant(i)) +' ' + sig;
-                
+
                 break;
-                
-            case org.apache.bcel.Constants.CONSTANT_InterfaceMethodref: case org.apache.bcel.Constants.CONSTANT_Methodref:
+
+            case org.apache.bcel.Constants.CONSTANT_InterfaceMethodref:
+            case org.apache.bcel.Constants.CONSTANT_Methodref:
             case org.apache.bcel.Constants.CONSTANT_Fieldref:
-                i = ((ConstantCP)c).getClassIndex();
-                v[i]=1;
-                j = ((ConstantCP)c).getNameAndTypeIndex();
-                v[j]=1;
+                i = ((ConstantCP) c).getClassIndex();
+                v[i] = 1;
+                j = ((ConstantCP) c).getNameAndTypeIndex();
+                v[j] = 1;
                 str = (constantToString(cp.getConstant(i)) + " " + constantToString(cp.getConstant(j)));
                 break;
-                
+
             default: // Never reached
                 throw new ClassFormatException("Unknown constant type " + tag);
         }
--- testdata/sources/java/Main.java
+++ testdata/sources/java/Main.java
@@ -1,12 +1,23 @@
 class Main {
 
-   public static void main(String[] argv) {
-      System.out.println("Started with the following arguments:");
-      for (int i =  0; i < argv.length; ++i) {
-         System.out.print('[');
-         System.out.print(argv[i]);
-         System.out.print("] ");
-      }
-      System.out.println();
-   }
+    private String[] argv;
+
+    public Main(String[] argv) {
+        this.argv = argv;
+    }
+
+    private void dump() {
+        System.out.println("Started with the following arguments:");
+        for (String s : argv) {
+            System.out.print('[');
+            System.out.print(s);
+            System.out.print("] ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] argv) {
+        Main main = new Main(argv);
+        main.dump();
+    }
 }
