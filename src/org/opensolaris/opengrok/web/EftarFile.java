bcd764f8cc90374497891e6ce649a4d8e552b78e
Jorgen Austvik <jorgen.austvik@sun.com>
2008-09-14 17:05:00 +0200

    Add back EftarFile main() method, used in run scripts

--- src/org/opensolaris/opengrok/web/EftarFile.java
+++ src/org/opensolaris/opengrok/web/EftarFile.java
@@ -35,6 +35,8 @@ import java.io.RandomAccessFile;
 import java.util.Map;
 import java.util.StringTokenizer;
 import java.util.TreeMap;
+import java.util.logging.Level;
+import org.opensolaris.opengrok.OpenGrokLogger;
 
 /**
  * An Extremely Fast Tagged Attribute Read-only File System
@@ -244,4 +246,26 @@ public class EftarFile {
         }
         write(args[args.length - 1]);
     }
+
+    /**
+     * Main method is used to generate eftar file from the path description
+     * file in the run scripts.
+     * 
+     * @param args Input files and output file
+     */
+    @SuppressWarnings("PMD.SystemPrintln")
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.err.println("Usage inputFile [inputFile ...] outputFile");
+            System.exit(1);
+        }
+
+        try {
+            EftarFile ef = new EftarFile();
+            ef.create(args);
+        } catch (Exception e) {
+            OpenGrokLogger.getLogger().log(Level.WARNING, "EftarFile error", e);
+        }
+    }
+
 }
