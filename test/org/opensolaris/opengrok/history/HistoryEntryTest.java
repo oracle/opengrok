3ab60e12b61afd78b31d65e86c97c73f09e3d6d2
Jorgen Austvik <jorgen.austvik@sun.com>
2008-10-11 20:32:50 +0200

    Fix copyrights

--- test/org/opensolaris/opengrok/history/HistoryEntryTest.java
+++ test/org/opensolaris/opengrok/history/HistoryEntryTest.java
@@ -1,8 +1,26 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * CDDL HEADER START
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
+ *
+ * See LICENSE.txt included in this distribution for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at LICENSE.txt.
+ * If applicable, add the following below this CDDL HEADER, with the
+ * fields enclosed by brackets "[]" replaced with your own identifying
+ * information: Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
  */
 
+/*
+ * Copyright 2008 Sun Microsystems, Inc.  All rights reserved.
+ * Use is subject to license terms.
+ */
 package org.opensolaris.opengrok.history;
 
 import java.util.ArrayList;
