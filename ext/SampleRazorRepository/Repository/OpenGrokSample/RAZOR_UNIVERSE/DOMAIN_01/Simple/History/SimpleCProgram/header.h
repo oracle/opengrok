a336a7912ecb62fb9310c4f3e50dc622aea4951c
Knut Anders Hatlen <Knut.Hatlen@Sun.COM>
2009-02-06 17:49:08 +0100

    Added sample Razor repository.
    Contributed by Peter Bray <Peter.Darren.Bray@gmail.com> (SCA OS0288).

---
+++ ext/SampleRazorRepository/Repository/OpenGrokSample/RAZOR_UNIVERSE/DOMAIN_01/Simple/History/SimpleCProgram/header.h
@@ -0,0 +1,4 @@
+INTRODUCE	pbray	1.1	Active	2009/01/22,09:32:39
+##TITLE:  Introduced file
+##ISSUE:	I...-..1     ++ISSUES++
+
---
+++ ext/SampleRazorRepository/Repository/OpenGrokSample/RAZOR_UNIVERSE/DOMAIN_01/Simple/History/SimpleCProgram/main.c
@@ -0,0 +1,16 @@
+INTRODUCE	pbray	1.1	Active	2009/01/22,09:32:39
+##TITLE:  Introduced file
+##ISSUE:	I...-..1     ++ISSUES++
+
+CHECK-OUT	pbray	1.1	Active	2009/01/22,09:36:02
+##TITLE:  Corrected Lint Errors
+##NOTES: CHECKED OUT TO: /fileserver/sandbox/pbray/OpenGrokSampleRepository
+##AUDIT: pbray 2009/01/22,09:36:02 1.1 Active CHECKED OUT TO: /fileserver/sandbox/pbray/OpenGrokSampleRepository/SimpleCProgram/main.c 
+##ISSUE:	I...-..2     ++ISSUES++
+
+CHECK-IN	pbray	1.2	Active	2009/01/22,09:36:41
+##TITLE:  Corrected Lint Errors
+##NOTES: CHECKED IN FROM: /fileserver/sandbox/pbray/OpenGrokSampleRepository
+##AUDIT: pbray 2009/01/22,09:36:41 1.2 Active CHECKED IN FROM: /fileserver/sandbox/pbray/OpenGrokSampleRepository/SimpleCProgram/main.c 
+##ISSUE:	I...-..2     ++ISSUES++
+
