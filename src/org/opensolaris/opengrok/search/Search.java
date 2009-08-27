01ed075d8c150456ed54889abb7d71f0f068d8ab
Trond Norbye <trond.norbye@sun.com>
2009-08-27 22:52:42 +0200

    Remove unused imports

--- src/org/opensolaris/opengrok/search/Search.java
+++ src/org/opensolaris/opengrok/search/Search.java
@@ -25,9 +25,7 @@ package org.opensolaris.opengrok.search;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
