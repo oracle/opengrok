14ed5f84701aa067da5676f1bfcae4156eb07f29
Knut Anders Hatlen <Knut.Hatlen@Sun.COM>
2010-04-05 15:33:10 +0200

    Fixed PMD warnings (Type: CollapsibleIfStatements)

--- src/org/opensolaris/opengrok/analysis/TextAnalyzer.java
+++ src/org/opensolaris/opengrok/analysis/TextAnalyzer.java
@@ -40,20 +40,18 @@ public abstract class TextAnalyzer extends FileAnalyzer {
     	byte[] head = new byte[3];
     	int br = in.read(head, 0, 3);
 
-    	if (br >= 2) {
-    		if ((head[0] == (byte)0xFE && head[1] == (byte)0xFF) || (head[0] == (byte)0xFF && (byte)head[1] == (byte)0xFE)) {
-    			charset = "UTF16";
-    			in.reset();	
-    		}
-    	}
-    	if (br >= 3) {
-    		if (head[0] == (byte)0xEF && head[1] == (byte)0xBB && head[2] == (byte)0xBF) {
-    			/* InputStreamReader does not properly discard BOM on UTF8 streams,
-    			 * so don't reset the stream. */ 
-    			charset = "UTF8";
-    		}
-    	}
-    	
+        if (br >= 2 &&
+                (head[0] == (byte)0xFE && head[1] == (byte)0xFF) ||
+                (head[0] == (byte)0xFF && head[1] == (byte)0xFE)) {
+            charset = "UTF-16";
+            in.reset();
+        } else if (br >= 3 & head[0] == (byte)0xEF && head[1] == (byte)0xBB &&
+                head[2] == (byte)0xBF) {
+            // InputStreamReader does not properly discard BOM on UTF8 streams,
+            // so don't reset the stream.
+            charset = "UTF-8";
+        }
+
     	if (charset == null) {
     		in.reset();
     		charset = Charset.defaultCharset().name();
