4607124840b147d00e82455576576d00b1161514
Trond Norbye <trond.norbye@gmail.com>
2011-05-13 20:33:08 +0200

    Fix file header and remove unused imports

--- test/org/opensolaris/opengrok/analysis/document/TroffAnalyzerTest.java
+++ test/org/opensolaris/opengrok/analysis/document/TroffAnalyzerTest.java
@@ -1,15 +1,20 @@
-/**
- * $Id$
+/*
+ * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
  * Common Development and Distribution License (the "License").
  * You may not use this file except in compliance with the License.
+ *
+ * See LICENSE.txt included in this distribution for the specific
+ * language governing permissions and limitations under the License.
  *
  * When distributing Covered Code, include this CDDL HEADER in each
  * file and include the License file at LICENSE.txt.
  * If applicable, add the following below this CDDL HEADER, with the
  * fields enclosed by brackets "[]" replaced with your own identifying
  * information: Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
  */
 /*
  * Copyright 2009 - 2011 Jens Elkner.
@@ -21,10 +26,7 @@ import static org.junit.Assert.*;
 import java.io.ByteArrayInputStream;
 import java.io.CharArrayWriter;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.StringReader;
 import java.io.StringWriter;
 
 import org.apache.lucene.document.Document;
