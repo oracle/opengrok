5fa2dfe6c5b3512e4b4181ec7a11d400a4837ed0
Trond Norbye <trond.norbye@gmail.com>
2012-06-19 11:04:57 +0200

    Reformat to remove checkstyle warnings
    The patch used a different source code style than we're using
    in OpenGrok. This patch reformat the source code to the style
    used in OpenGrok (simple reformat code in NetBeans)

--- src/org/opensolaris/opengrok/history/AccuRevHistoryParser.java
+++ src/org/opensolaris/opengrok/history/AccuRevHistoryParser.java
@@ -20,7 +20,6 @@
 /*
  * Copyright (c) 2008, 2010, Oracle and/or its affiliates. All rights reserved.
  */
-
 package org.opensolaris.opengrok.history;
 
 import java.io.BufferedReader;
@@ -28,13 +27,12 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
-import java.text.DateFormat;
-import java.text.ParseException;
-
 import org.opensolaris.opengrok.OpenGrokLogger;
 import org.opensolaris.opengrok.util.Executor;
 
@@ -57,125 +55,130 @@ public class AccuRevHistoryParser implements Executor.StreamHandler {
      * @return object representing the file's history
      * @throws HistoryException if a problem occurs while executing p4 command
      */
-    History parse ( File file, Repository repos ) throws HistoryException {
+    History parse(File file, Repository repos) throws HistoryException {
 
         repository = (AccuRevRepository) repos;
-        
+
         history = null;
         foundHistory = false;
 
-        String relPath = repository.getDepotRelativePath( file );
+        String relPath = repository.getDepotRelativePath(file);
 
-        /*------------------------------------------------------
-        * When the path given is really just the root to the
-        * source workarea, no history is available, create fake.
-        *------------------------------------------------------*/
-        if ( relPath.equals( "/./" ) ) {
+        /*
+         * ------------------------------------------------------ When the path
+         * given is really just the root to the source workarea, no history is
+         * available, create fake.
+        *------------------------------------------------------
+         */
+        if (relPath.equals("/./")) {
 
             List<HistoryEntry> entries = new ArrayList<HistoryEntry>();
-                
-            entries.add( new HistoryEntry(
-                "", new Date(),"OpenGrok", "Workspace Root", true) );
 
-            history = new History( entries );
-            
+            entries.add(new HistoryEntry(
+                    "", new Date(), "OpenGrok", "Workspace Root", true));
+
+            history = new History(entries);
+
         } else {
             try {
-                /*-------------------------------------------------------------
-                * Errors will be logged, so not bothering to add to the output.
-                *-------------------------------------------------------------*/
-                Executor executor = repository.getHistoryLogExecutor( file );
-                executor.exec( true, this );
-                
-                /*----------------------------------------------
-                * Try again because there was no 'keep' history.
-                *----------------------------------------------*/
-                if ( ! foundHistory ) {
-                    executor = repository.getHistoryLogExecutor( file );
-                    executor.exec( true, this );
+                /*
+                 * -------------------------------------------------------------
+                 * Errors will be logged, so not bothering to add to the output.
+                *-------------------------------------------------------------
+                 */
+                Executor executor = repository.getHistoryLogExecutor(file);
+                executor.exec(true, this);
+
+                /*
+                 * ---------------------------------------------- Try again
+                 * because there was no 'keep' history.
+                *----------------------------------------------
+                 */
+                if (!foundHistory) {
+                    executor = repository.getHistoryLogExecutor(file);
+                    executor.exec(true, this);
                 }
-            }
-            catch ( IOException e ) {
+            } catch (IOException e) {
                 throw new HistoryException(
-                      "Failed to get history for: \"" +
-                      file.getAbsolutePath() + "\"" + e );
+                        "Failed to get history for: \""
+                        + file.getAbsolutePath() + "\"" + e);
             }
         }
-        
+
         return history;
     }
 
-    public void processStream( InputStream input ) throws IOException {
-    
-        BufferedReader in = new BufferedReader( new InputStreamReader( input ));
+    public void processStream(InputStream input) throws IOException {
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(input));
         List<HistoryEntry> entries = new ArrayList<HistoryEntry>();
         DateFormat df = repository.getDateFormat();
         String line;
         String user;
         Date date;
         HistoryEntry entry = null;
-        
+
         history = new History();
-        
-        /*---------------------------------------------------------------
-        * Accurev history of an element (directory or file) looks like:
-        * 
-        * element: /./path/to/element
-        * eid: 238865
-        * transaction 1486194; purge; 2012/02/28 12:46:55 ; user: tluksha
-        *   version 2541/1 (2539/1)
-        * 
-        * transaction 1476285; purge; 2012/02/03 12:16:25 ; user: shaehn
-        *   version 4241/1 (4241/1)
-        *   
-        * transaction 1317224; promote; 2011/05/03 11:37:56 ; user: mtrinh
-        *   # changes to ngb to compile on windows
-        *   version 13/93 (1000/2)
-        *
-        * ...
-        *
-        * What is of interest then is:
-        *  user (author), version (revision), date, comment (message)
-        *
-        * Any lines not recognized are ignored.
-        *---------------------------------------------------------------*/
-        while ( (line = in.readLine()) != null ) {
 
-            if ( line.startsWith( "e" )) { continue; } // ignore element, eid
-            
-            if ( line.startsWith( "t" )) {             // found transaction
-                
-                String data[] = line.split( "; " );
+        /*
+         * ---------------------------------------------------------------
+         * Accurev history of an element (directory or file) looks like:
+         *
+         * element: /./path/to/element eid: 238865 transaction 1486194; purge;
+         * 2012/02/28 12:46:55 ; user: tluksha version 2541/1 (2539/1)
+         *
+         * transaction 1476285; purge; 2012/02/03 12:16:25 ; user: shaehn
+         * version 4241/1 (4241/1)
+         *
+         * transaction 1317224; promote; 2011/05/03 11:37:56 ; user: mtrinh #
+         * changes to ngb to compile on windows version 13/93 (1000/2)
+         *
+         * ...
+         *
+         * What is of interest then is: user (author), version (revision), date,
+         * comment (message)
+         *
+         * Any lines not recognized are ignored.
+        *---------------------------------------------------------------
+         */
+        while ((line = in.readLine()) != null) {
+
+            if (line.startsWith("e")) {
+                continue;
+            } // ignore element, eid
+
+            if (line.startsWith("t")) {             // found transaction
+
+                String data[] = line.split("; ");
                 entry = new HistoryEntry();
-                
-                foundHistory = true;
-                user = data[3].replaceFirst( "user: ", "" );
-                
-                entry.setMessage( "" );
-                entry.setAuthor( user );
-                 
-                try {
-                    date = df.parse( data[2] );
-                    entry.setDate( date );
-                }
-                catch ( ParseException pe ) {
-                    OpenGrokLogger.getLogger().log(
-                      Level.WARNING, "Could not parse date: " + line, pe );
-                }
-               
-            } else if ( line.startsWith( "  #" ) ) {  // found comment
-            
-                entry.appendMessage( line.substring( 3 ));
 
-            } else if ( line.startsWith( "  v" ) ) {  // found version
-            
-                String data[] = line.split( "\\s+" );
-                entry.setRevision( data[2] );
-                entry.setActive( true );
-                entries.add( entry );
+                foundHistory = true;
+                user = data[3].replaceFirst("user: ", "");
+
+                entry.setMessage("");
+                entry.setAuthor(user);
+
+                try {
+                    date = df.parse(data[2]);
+                    entry.setDate(date);
+                } catch (ParseException pe) {
+                    OpenGrokLogger.getLogger().log(
+                            Level.WARNING, "Could not parse date: " + line, pe);
+                }
+
+            } else if (line.startsWith("  #")) {  // found comment
+
+                entry.appendMessage(line.substring(3));
+
+            } else if (line.startsWith("  v")) {  // found version
+
+                String data[] = line.split("\\s+");
+                entry.setRevision(data[2]);
+                entry.setActive(true);
+                entries.add(entry);
             }
         }
-        
-        history.setHistoryEntries( entries );
+
+        history.setHistoryEntries(entries);
     }
 }
--- src/org/opensolaris/opengrok/history/AccuRevRepository.java
+++ src/org/opensolaris/opengrok/history/AccuRevRepository.java
@@ -31,86 +31,84 @@ import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 import org.opensolaris.opengrok.OpenGrokLogger;
+import org.opensolaris.opengrok.configuration.RuntimeEnvironment;
 import org.opensolaris.opengrok.util.Executor;
 import org.opensolaris.opengrok.util.IOUtils;
-import org.opensolaris.opengrok.configuration.RuntimeEnvironment;
 
 /**
  * Access to an AccuRev repository (here an actual user workspace)
  *
- * AccuRev requires that a user logs into their system before
- * it can be used. So on the machine acting as the OpenGrok
- * server, some valid user has to be permanently logged in.
- * (accurev login -n <user>)
+ * AccuRev requires that a user logs into their system before it can be used. So
+ * on the machine acting as the OpenGrok server, some valid user has to be
+ * permanently logged in. (accurev login -n <user>)
  *
- * It appears that the file path that is given to all these methods
- * is the complete path to the file which includes the path to the
- * root of the source location. This means that when using the -P
- * option of OpenGrok to make all the directories pointed to by the
- * source root to be seen as separate projects is not all as it would
- * seem. The History GURU always starts building the history cache
- * using the source root. Well there is NO HISTORY for anything at
- * the source root because it is not part of an actual AccuRev depot.
- * The directories within the source root directory represent the 
- * work areas of AccuRev and it is those areas where history can be
- * obtained. This implementation allows those directories to be
- * symbolic links to the actual workspaces.
+ * It appears that the file path that is given to all these methods is the
+ * complete path to the file which includes the path to the root of the source
+ * location. This means that when using the -P option of OpenGrok to make all
+ * the directories pointed to by the source root to be seen as separate projects
+ * is not all as it would seem. The History GURU always starts building the
+ * history cache using the source root. Well there is NO HISTORY for anything at
+ * the source root because it is not part of an actual AccuRev depot. The
+ * directories within the source root directory represent the work areas of
+ * AccuRev and it is those areas where history can be obtained. This
+ * implementation allows those directories to be symbolic links to the actual
+ * workspaces.
  *
  * Other assumptions:
  *
- *  There is only one associated AccuRev depot associated with workspaces.
+ * There is only one associated AccuRev depot associated with workspaces.
  *
  * @author Steven Haehn
  */
 public class AccuRevRepository extends Repository {
 
     private static final long serialVersionUID = 1L;
-
-    /** The property name used to obtain the client command for this repository. */
+    /**
+     * The property name used to obtain the client command for this repository.
+     */
     public static final String CMD_PROPERTY_KEY =
-        "org.opensolaris.opengrok.history.AccuRev";
-
-    /** The command to use to access the repository if none was given explicitly */
+            "org.opensolaris.opengrok.history.AccuRev";
+    /**
+     * The command to use to access the repository if none was given explicitly
+     */
     public static final String CMD_FALLBACK = "accurev";
-
     private static final Pattern annotationPattern =
-        Pattern.compile("^\\s+(\\d+/\\d+)\\s+(\\w+)");   // version, user
-
+            Pattern.compile("^\\s+(\\d+/\\d+)\\s+(\\w+)");   // version, user
     private static final Pattern depotPattern =
-        Pattern.compile( "^Depot:\\s+(\\w+)" );
-        
+            Pattern.compile("^Depot:\\s+(\\w+)");
     private static final RuntimeEnvironment env = RuntimeEnvironment.getInstance();
-    
+
     public AccuRevRepository() {
         type = "AccuRev";
         datePattern = "yyyy/MM/dd hh:mm:ss";
-        ensureCommand( CMD_PROPERTY_KEY, CMD_FALLBACK );
+        ensureCommand(CMD_PROPERTY_KEY, CMD_FALLBACK);
     }
 
     @Override
     public Annotation annotate(File file, String rev) throws IOException {
 
-        Annotation a = new Annotation( file.getName() );
+        Annotation a = new Annotation(file.getName());
 
         ArrayList<String> cmd = new ArrayList<String>();
-        
-        /*-----------------------------------------------
-        * Strip off source root to get to workspace path.
-        *-----------------------------------------------*/
-        String path = getDepotRelativePath( file );
-                
+
+        /*
+         * ----------------------------------------------- Strip off source root
+         * to get to workspace path.
+        *-----------------------------------------------
+         */
+        String path = getDepotRelativePath(file);
+
         cmd.add(this.cmd);
         cmd.add("annotate");
         cmd.add("-fvu");      // version & user
-        
-        if ( rev != null ) {
-            cmd.add( "-v" );
-            cmd.add( rev.trim() );
+
+        if (rev != null) {
+            cmd.add("-v");
+            cmd.add(rev.trim());
         }
-        
-        cmd.add( path );
+
+        cmd.add(path);
 
         Executor executor = new Executor(cmd, file.getParentFile());
         executor.exec();
@@ -122,53 +120,55 @@ public class AccuRevRepository extends Repository {
             while ((line = reader.readLine()) != null) {
                 ++lineno;
                 Matcher matcher = annotationPattern.matcher(line);
-                
+
                 if (matcher.find()) {
                     String version = matcher.group(1);
-                    String author  = matcher.group(2);
+                    String author = matcher.group(2);
                     a.addLine(version, author, true);
                 } else {
                     OpenGrokLogger.getLogger().log(Level.SEVERE,
-                        "Did not find annotation in line "
-                        + lineno + ": [" + line + "]");
+                            "Did not find annotation in line "
+                            + lineno + ": [" + line + "]");
                 }
             }
         } catch (IOException e) {
             OpenGrokLogger.getLogger().log(Level.SEVERE,
                     "Could not read annotations for " + file, e);
         }
-                
+
         IOUtils.close(reader);
         return a;
     }
 
     /**
-     * Get an executor to be used for retrieving the 
-     * history log for the given file. (used by AccuRevHistoryParser).
+     * Get an executor to be used for retrieving the history log for the given
+     * file. (used by AccuRevHistoryParser).
      *
      * @param file file for which history is to be retrieved.
      * @return An Executor ready to be started
      */
-    Executor getHistoryLogExecutor( File file ) throws IOException {
+    Executor getHistoryLogExecutor(File file) throws IOException {
 
-        /*-----------------------------------------------
-        * Strip off source root to get to workspace path.
-        *-----------------------------------------------*/
-        String path = getDepotRelativePath( file );
+        /*
+         * ----------------------------------------------- Strip off source root
+         * to get to workspace path.
+        *-----------------------------------------------
+         */
+        String path = getDepotRelativePath(file);
 
         ArrayList<String> cmd = new ArrayList<String>();
-        
-        cmd.add( this.cmd );
-        cmd.add( "hist" );
-        
-        if ( ! file.isDirectory() ) {
-            cmd.add( "-k" );
-            cmd.add( "keep" );  // get a list of all 'real' file versions
-        }
-        
-        cmd.add( path );
 
-        return new Executor( cmd, file.getParentFile() );
+        cmd.add(this.cmd);
+        cmd.add("hist");
+
+        if (!file.isDirectory()) {
+            cmd.add("-k");
+            cmd.add("keep");  // get a list of all 'real' file versions
+        }
+
+        cmd.add(path);
+
+        return new Executor(cmd, file.getParentFile());
     }
 
     @Override
@@ -176,69 +176,71 @@ public class AccuRevRepository extends Repository {
 
         ArrayList<String> cmd = new ArrayList<String>();
         InputStream inputStream = null;
-        File directory = new File( parent );
-        
-        /*-----------------------------------------------------------------
-        * The only way to guarantee getting the contents of a file is to
-        * fire off an AccuRev 'stat'us command to get the element ID
-        * number for the subsequent 'cat' command. (Element ID's are unique
-        * for a file, unless evil twins are present) This is because it is
-        * possible that the file may have been moved to a different place
-        * in the depot. The 'stat' command will produce a line with the
-        * format:  
-        *
-        *  <filePath> <elementID> <virtualVersion> (<realVersion>) (<status>)
-        *
-        *  /./myFile   e:17715     CP.73_Depot/2     (3220/2)       (backed)
-        *-----------------------------------------------------------------*/
-        cmd.add( this.cmd );
-        cmd.add( "stat" );
-        cmd.add( "-fe" );
-        cmd.add( basename );
-        Executor executor = new Executor( cmd, directory );
+        File directory = new File(parent);
+
+        /*
+         * ----------------------------------------------------------------- The
+         * only way to guarantee getting the contents of a file is to fire off
+         * an AccuRev 'stat'us command to get the element ID number for the
+         * subsequent 'cat' command. (Element ID's are unique for a file, unless
+         * evil twins are present) This is because it is possible that the file
+         * may have been moved to a different place in the depot. The 'stat'
+         * command will produce a line with the format:
+         *
+         * <filePath> <elementID> <virtualVersion> (<realVersion>) (<status>)
+         *
+         *  /./myFile e:17715 CP.73_Depot/2 (3220/2) (backed)
+        *-----------------------------------------------------------------
+         */
+        cmd.add(this.cmd);
+        cmd.add("stat");
+        cmd.add("-fe");
+        cmd.add(basename);
+        Executor executor = new Executor(cmd, directory);
         executor.exec();
 
-        BufferedReader info = new BufferedReader( executor.getOutputReader() );
+        BufferedReader info = new BufferedReader(executor.getOutputReader());
         String elementID = null;
         String line;
 
         try {
             line = info.readLine();
-            
-            String [] statInfo = line.split( "\\s+" );
-            elementID = statInfo[1].substring( 2 ); // skip over 'e:'
-            
+
+            String[] statInfo = line.split("\\s+");
+            elementID = statInfo[1].substring(2); // skip over 'e:'
+
+        } catch (IOException e) {
+            OpenGrokLogger.getLogger().log(Level.SEVERE,
+                    "Could not obtain status for " + basename);
         }
-        catch ( IOException e ) {
-            OpenGrokLogger.getLogger().log( Level.SEVERE,
-                "Could not obtain status for " + basename );
-        }
-        
-        if ( elementID != null ) {
-            /*------------------------------------------
-            * This really gets the contents of the file.
-            *------------------------------------------*/
+
+        if (elementID != null) {
+            /*
+             * ------------------------------------------ This really gets the
+             * contents of the file.
+            *------------------------------------------
+             */
             cmd.clear();
-            cmd.add( this.cmd );
-            cmd.add( "cat" );
-            cmd.add( "-v" );
-            cmd.add( rev.trim() );
-            cmd.add( "-e" );
-            cmd.add( elementID );
-            
-            executor = new Executor( cmd, directory );
+            cmd.add(this.cmd);
+            cmd.add("cat");
+            cmd.add("-v");
+            cmd.add(rev.trim());
+            cmd.add("-e");
+            cmd.add(elementID);
+
+            executor = new Executor(cmd, directory);
             executor.exec();
-            
-            inputStream = 
-              new ByteArrayInputStream(executor.getOutputString().getBytes());
+
+            inputStream =
+                    new ByteArrayInputStream(executor.getOutputString().getBytes());
         }
-        
+
         return inputStream;
     }
-    
+
     @Override
     public void update() throws IOException {
-        throw new UnsupportedOperationException( "Not supported yet." );
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
@@ -250,93 +252,91 @@ public class AccuRevRepository extends Repository {
     public boolean fileHasAnnotation(File file) {
         return true;
     }
-        
+
     /**
      * Check if a given path is associated with an AccuRev workspace
      *
-     *   The AccuRev 'info' command provides a Depot name when in a
-     *   known workspace. Otherwise, the Depot name will be missing.
+     * The AccuRev 'info' command provides a Depot name when in a known
+     * workspace. Otherwise, the Depot name will be missing.
      *
      * @param path The presumed path to an AccuRev workspace directory.
      * @return true if the given path is in the depot, false otherwise
      */
-    private boolean isInAccuRevDepot( File wsPath ) {
-    
+    private boolean isInAccuRevDepot(File wsPath) {
+
         boolean status = false;
-        
+
         if (isWorking()) {
             ArrayList<String> cmd = new ArrayList<String>();
 
             cmd.add(this.cmd);
-            cmd.add( "info" );
+            cmd.add("info");
 
-            Executor executor = new Executor( cmd, wsPath );
-            executor.exec( true );
+            Executor executor = new Executor(cmd, wsPath);
+            executor.exec(true);
 
-            BufferedReader info = new BufferedReader( executor.getOutputReader() );
+            BufferedReader info = new BufferedReader(executor.getOutputReader());
             String line;
 
             try {
-                while ( (line = info.readLine()) != null ) {
+                while ((line = info.readLine()) != null) {
 
-                    Matcher depotMatch = depotPattern.matcher( line );
+                    Matcher depotMatch = depotPattern.matcher(line);
 
-                    if ( line.indexOf( "not logged in" ) != -1 ) {
-                        OpenGrokLogger.getLogger().log( 
-                            Level.SEVERE, "Not logged into AccuRev server" );
+                    if (line.indexOf("not logged in") != -1) {
+                        OpenGrokLogger.getLogger().log(
+                                Level.SEVERE, "Not logged into AccuRev server");
                         break;
                     }
-                    
-                    if ( depotMatch.find() ) {
+
+                    if (depotMatch.find()) {
                         status = true;
                         break;
                     }
                 }
-            }
-            catch ( IOException e ) {
-                OpenGrokLogger.getLogger().log( Level.SEVERE,
-                    "Could not find AccuRev repository for " + wsPath );
+            } catch (IOException e) {
+                OpenGrokLogger.getLogger().log(Level.SEVERE,
+                        "Could not find AccuRev repository for " + wsPath);
             }
         }
 
         return status;
     }
 
-    public String getDepotRelativePath( File file ) {
-    
+    public String getDepotRelativePath(File file) {
+
         String path = "/./";
-        
+
         try {
-            path = env.getPathRelativeToSourceRoot( file, 0 );
-            
-            if ( path.startsWith( "/" ) ) {
+            path = env.getPathRelativeToSourceRoot(file, 0);
+
+            if (path.startsWith("/")) {
                 path = "/." + path;
-            }  else {
+            } else {
                 path = "/./" + path;
             }
+        } catch (IOException e) {
+            OpenGrokLogger.getLogger().log(Level.WARNING,
+                    "Unable to determine depot relative path for " + file.getPath());
         }
-        catch ( IOException e ) {
-            OpenGrokLogger.getLogger().log( Level.WARNING,
-                 "Unable to determine depot relative path for " + file.getPath() );
-        }
-        
+
         return path;
     }
-    
+
     @Override
-    boolean isRepositoryFor( File sourceHome ) {
-            
-        return isInAccuRevDepot( sourceHome );
+    boolean isRepositoryFor(File sourceHome) {
+
+        return isInAccuRevDepot(sourceHome);
     }
 
     @Override
     public boolean isWorking() {
-    
+
         if (working == null) {
 
-            working = checkCmd( cmd, "info" );
+            working = checkCmd(cmd, "info");
         }
-        
+
         return working.booleanValue();
     }
 
