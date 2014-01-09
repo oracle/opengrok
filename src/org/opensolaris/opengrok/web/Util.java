24d215638bd1ea04d5186d63d7c722a7525b720c
Vladimir Kotal <Vladimir.Kotal@Oracle.COM>
2014-01-09 14:45:50 +0100

    first version of changeset search link in Annotate view fixes #721

--- src/org/opensolaris/opengrok/web/Util.java
+++ src/org/opensolaris/opengrok/web/Util.java
@@ -505,11 +505,11 @@ public final class Util {
             out.write("<span class=\"blame\">");
             if (enabled) {
                 out.write(anchorClassStart);
-                out.write("r\" href=\"" );
+                out.write("r\" href=\"");
                 out.write(URIEncode(annotation.getFilename()));
                 out.write("?a=true&amp;r=");
                 out.write(URIEncode(r));
-                String msg = annotation.getDesc(r);
+		String msg = annotation.getDesc(r);
                 if (msg != null) {
                     out.write("\" title=\"");
                     out.write(msg);
@@ -521,6 +521,15 @@ public final class Util {
             out.write(buf.toString());
             buf.setLength(0);
             if (enabled) {
+                out.write(anchorEnd);
+
+                // Write link to search the revision.
+                out.write(anchorClassStart);
+                out.write("search\" href=\"/source/search?q=&defs=&refs=&path=&hist="); // XXX do not hardcode source
+                out.write(URIEncode(r));
+                out.write("&type=\" title=\"Search history for this changeset");
+                out.write(closeQuotedTag);
+                out.write("S");
                 out.write(anchorEnd);
             }
             String a = annotation.getAuthor(num);
