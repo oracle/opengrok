2db89d1085d2fc035e7b634555a002b696898ddf
Sebastian Kreft <skreft@gmail.com>
2014-03-10 23:23:07 +0100

    Modified web/UtilTest

--- test/org/opensolaris/opengrok/web/UtilTest.java
+++ test/org/opensolaris/opengrok/web/UtilTest.java
@@ -128,7 +128,8 @@ public class UtilTest {
         StringWriter out = new StringWriter();
         // hmmm - where do meaningful test start?
         Util.readableLine(42, out, null, null, null);
-        assertEquals("\n<a class=\"l\" name=\"42\" href=\"#42\">42</a>",
+        assertEquals("\n<div id=\"l42\" class=\"line-bg\"></div>"
+                     + "<a class=\"l\" name=\"42\" href=\"#42\">42</a>",
                      out.toString());
 
         out.getBuffer().setLength(0); // clear buffer
