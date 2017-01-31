fbbda5450956c9c078297dd6b9be51a7381b63b0
Vladimir Kotal <Vladimir.Kotal@Oracle.COM>
2017-01-31 16:35:42 +0100

    copyright

--- test/org/opensolaris/opengrok/history/FileHistoryCacheTest.java
+++ test/org/opensolaris/opengrok/history/FileHistoryCacheTest.java
@@ -18,7 +18,7 @@
  */
 
 /*
- * Copyright (c) 2014, 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2017, Oracle and/or its affiliates. All rights reserved.
  */
 package org.opensolaris.opengrok.history;
 
