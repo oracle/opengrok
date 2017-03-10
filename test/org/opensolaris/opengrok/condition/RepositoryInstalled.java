7bc521cffdfb4d16305d73a8c05e1493c34a1001
Vladimir Kotal <Vladimir.Kotal@Oracle.COM>
2017-03-10 20:43:04 +0100

    copyright

--- test/org/opensolaris/opengrok/condition/RepositoryInstalled.java
+++ test/org/opensolaris/opengrok/condition/RepositoryInstalled.java
@@ -17,8 +17,8 @@
  * CDDL HEADER END
  */
 
- /*
- * Copyright (c) 2016, Oracle and/or its affiliates. All rights reserved.
+/*
+ * Copyright (c) 2016, 2017, Oracle and/or its affiliates. All rights reserved.
  */
 package org.opensolaris.opengrok.condition;
 
