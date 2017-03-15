4421710bec996d163f5b17df4d87b1149770bd55
Lubos Kosco <tarzanek@gmail.com>
2017-03-15 14:53:49 +0100

    add test for long token splitting for plainsymboltokenizer on js file

--- test/org/opensolaris/opengrok/search/SearchTest.java
+++ test/org/opensolaris/opengrok/search/SearchTest.java
@@ -274,4 +274,26 @@ public class SearchTest {
         assertTrue(array.toString().contains("Makefile: [...]"));
         System.setOut(out);
     }
+    
+    /**
+     * Test of long line indexing/splitting for plain symbol tokenizer.
+     */
+    @Test
+    public void testJavascriptLongLine() {
+        if (skip) {
+            return;
+        }
+        
+        Search instance = new Search();
+        
+        assertTrue(instance.parseCmdLine(new String[]{"-f", "\"beforelongline\"","-p", "\"testlong.js\""}));
+        assertTrue(instance.search());
+        assertEquals(1, instance.results.size());
+        
+        //if fix for #1170 works, below should be also in index
+        assertTrue(instance.parseCmdLine(new String[]{"-f", "\"afterlongline\"","-p", "\"testlong.js\""}));
+        assertTrue(instance.search());
+        assertEquals(1, instance.results.size());
+    }
+    
 }
