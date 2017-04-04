b8a6d825ddb7ea7bf6cf1bb2f82829361f243890
Lubos Kosco <tarzanek@gmail.com>
2017-04-04 18:03:23 +0200

    fix bitkeeper download location due to expired ssl certs

--- scripts/install-bitkeeper.sh
+++ scripts/install-bitkeeper.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/bin/sh -x
 
 if [ `uname -m` = "x86_64" ]; then
-    curl -o bkinst.bin https://www.bitkeeper.com/downloads/7.3.1ce/bk-7.3.1ce-x86_64-glibc219-linux.bin
+    curl -o bkinst.bin http://www.bitkeeper.org/downloads/7.3.1ce/bk-7.3.1ce-x86_64-glibc219-linux.bin
 else
-    curl -o bkinst.bin https://www.bitkeeper.com/downloads/7.3.1ce/bk-7.3.1ce-x86-glibc219-linux.bin
+    curl -o bkinst.bin http://www.bitkeeper.org/downloads/7.3.1ce/bk-7.3.1ce-x86-glibc219-linux.bin
 fi
 
 chmod +x bkinst.bin
