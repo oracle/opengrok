bedee32b077d8a164f02500bd36a20c2afe6c6cb
Josh Soref <jsoref@users.noreply.github.com>
2017-05-16 03:00:06 +0000

    spelling: iauthorization

--- src/org/opensolaris/opengrok/authorization/AuthorizationPlugin.java
+++ src/org/opensolaris/opengrok/authorization/AuthorizationPlugin.java
@@ -208,7 +208,7 @@ public class AuthorizationPlugin extends AuthorizationStack {
         try {
             /**
              * The exception should not happen here as we already have an
-             * instance of IAuthoriazationPlugin. But it is required by the
+             * instance of IAuthorizationPlugin. But it is required by the
              * compiler.
              *
              * NOTE: If we were to add a throws clause here we would interrupt
