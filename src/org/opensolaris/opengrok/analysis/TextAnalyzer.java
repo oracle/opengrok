93e370b9c803ea37a3791dbdd52f8de5a6095dd3
tetsurom <matsumura.t.lab@gmail.com>
2017-06-09 15:35:22 +0900

    Recognize UTF-8/16 BOM in annotate, history and diff same as in xref.

--- src/org/opensolaris/opengrok/analysis/TextAnalyzer.java
+++ src/org/opensolaris/opengrok/analysis/TextAnalyzer.java
@@ -28,6 +28,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.nio.charset.Charset;
+import org.opensolaris.opengrok.util.IOUtils;
 
 public abstract class TextAnalyzer extends FileAnalyzer {
 
@@ -36,33 +37,6 @@ public abstract class TextAnalyzer extends FileAnalyzer {
     }
 
     protected Reader getReader(InputStream stream) throws IOException {
-        InputStream in = stream.markSupported() ?
-                stream : new BufferedInputStream(stream);
-
-        String charset = null;
-
-        in.mark(3);
-
-        byte[] head = new byte[3];
-        int br = in.read(head, 0, 3);
-
-        if (br >= 2
-                && (head[0] == (byte) 0xFE && head[1] == (byte) 0xFF)
-                || (head[0] == (byte) 0xFF && head[1] == (byte) 0xFE)) {
-            charset = "UTF-16";
-            in.reset();
-        } else if (br >= 3 && head[0] == (byte) 0xEF && head[1] == (byte) 0xBB
-                && head[2] == (byte) 0xBF) {
-            // InputStreamReader does not properly discard BOM on UTF8 streams,
-            // so don't reset the stream.
-            charset = "UTF-8";
-        }
-
-        if (charset == null) {
-            in.reset();
-            charset = Charset.defaultCharset().name();
-        }
-
-        return new InputStreamReader(in, charset);
+        return IOUtils.createBOMStrippedReader(stream);
     }
 }
