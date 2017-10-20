73cc18fe5e6222c474912580862e9981c5d48be8
Vladimir Kotal <vlada@devnull.cz>
2017-10-20 20:58:42 +0200

    restore tag of the message that sets projectsEnabled
    fixes #1838

--- src/org/opensolaris/opengrok/index/Indexer.java
+++ src/org/opensolaris/opengrok/index/Indexer.java
@@ -229,7 +229,7 @@ public final class Indexer {
             // emitted during indexing do not cause validation error.
             if (addProjects && host != null && port > 0) {
                 Message m = Message.createMessage("config");
-                m.addTag("addOption");
+                m.addTag("set");
                 m.setText("projectsEnabled = true");
                 m.write(host, port);
             }
