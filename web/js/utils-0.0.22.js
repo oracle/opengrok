38f7757331b514eb0f46906ebcfbfdbdc6b4128d
Adam Hornáček <adam.hornacek@icloud.com>
2018-05-30 08:56:24 +0200

    Update jQuery UI to 1.12.1

--- web/js/utils-0.0.22.js
+++ web/js/utils-0.0.22.js
@@ -18,7 +18,7 @@
  */
 
 /*
- * Copyright (c) 2009, 2017, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2009, 2018, Oracle and/or its affiliates. All rights reserved.
  * Portions Copyright 2011 Jens Elkner.
  * Portions Copyright (c) 2017, Chris Fraire <cfraire@me.com>.
  */
@@ -508,7 +508,7 @@
                 title: 'Window',
                 appendDraggable: '#content',
                 draggable: true,
-                draggableScript: 'js/jquery-ui-1.12.0-draggable.min.js', // relative to context
+                draggableScript: 'js/jquery-ui-1.12.1-draggable.min.js', // relative to context
                 contextPath: browserWindow.contextPath,
                 parent: undefined,
                 load: undefined,
