496029da05ffb58eb848fc466e183000b6b64ced
Vladimir Kotal <vlada@devnull.cz>
2018-06-27 11:13:43 +0200

    disable part of testSearch() because of intermittent failures (#2030)

--- test/org/opensolaris/opengrok/search/SearchTest.java
+++ test/org/opensolaris/opengrok/search/SearchTest.java
@@ -145,9 +145,11 @@ public class SearchTest {
         assertTrue(instance.search());
         assertEquals(1, instance.results.size());
 
+        /* see https://github.com/oracle/opengrok/issues/2030
         assertTrue(instance.parseCmdLine(new String[]{"-p", "main~"}));
         assertTrue(instance.search());
         assertEquals("Search for main~ in testdata sources", 9, instance.results.size()); // only in SearchEngine ... why?
+        */
 
         assertTrue(instance.parseCmdLine(new String[]{"-p", "\"main troff\"~5"}));
         assertTrue(instance.search());
