2f5928718325ec15fc6e1b2523ac7dad93eb6399
Adam Hornáček <adam.hornacek@icloud.com>
2018-06-30 12:45:18 +0200

    Store phrase scorer in local variable to allow parallel access

--- suggester/src/main/java/org/opengrok/suggest/query/data/BitIntsHolder.java
+++ suggester/src/main/java/org/opengrok/suggest/query/data/BitIntsHolder.java
@@ -4,6 +4,13 @@ import java.util.BitSet;
 
 public class BitIntsHolder extends BitSet implements IntsHolder {
 
+    public BitIntsHolder() {
+    }
+
+    public BitIntsHolder(int nbits) {
+        super(nbits);
+    }
+
     @Override
     public boolean has(final int i) {
         return get(i);
