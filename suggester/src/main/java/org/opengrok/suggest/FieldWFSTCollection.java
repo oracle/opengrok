51846170d596de0f8750646a42affc3cb3f6feaa
Adam Hornáček <adam.hornacek@icloud.com>
2018-07-01 16:23:28 +0200

    Remove suggester data recursively

--- suggester/src/main/java/org/opengrok/suggest/FieldWFSTCollection.java
+++ suggester/src/main/java/org/opengrok/suggest/FieldWFSTCollection.java
@@ -41,9 +41,12 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -230,8 +233,36 @@ class FieldWFSTCollection implements Closeable {
 
     public void remove() {
         close();
-        boolean deleteSuccessful = suggesterDir.toFile().delete();
-        if (!deleteSuccessful) {
+
+        // TODO: copy from opengrok -> maybe create shared utils module?
+        try {
+            Files.walkFileTree(suggesterDir, new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
+                        throws IOException {
+                    Files.delete(file);
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+                    // Try to delete the file anyway.
+                    Files.delete(file);
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                    if (exc == null) {
+                        Files.delete(dir);
+                        return FileVisitResult.CONTINUE;
+                    } else {
+                        // Directory traversal failed.
+                        throw exc;
+                    }
+                }
+            });
+        } catch (IOException e) {
             logger.log(Level.WARNING, "Cannot remove suggester data: {0}", suggesterDir);
         }
     }
