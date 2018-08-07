fb1c4de07bbea1ec8e81c98217ecc6952967bc33
Vladimir Kotal <vlada@devnull.cz>
2018-08-07 11:00:15 +0200

    remove unused import

--- opengrok-indexer/src/main/java/org/opengrok/indexer/history/SSCMRepository.java
+++ opengrok-indexer/src/main/java/org/opengrok/indexer/history/SSCMRepository.java
@@ -31,7 +31,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.Reader;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
