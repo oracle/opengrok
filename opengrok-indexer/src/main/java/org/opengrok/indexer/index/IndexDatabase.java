9489792cb0e082690dcdb2d5d9ed5125c1d20c86
Vladimir Kotal <vlada@devnull.cz>
2018-08-16 16:07:33 +0200

    fix path construction (#2299)

--- opengrok-indexer/src/main/java/org/opengrok/indexer/index/IndexDatabase.java
+++ opengrok-indexer/src/main/java/org/opengrok/indexer/index/IndexDatabase.java
@@ -1077,7 +1077,7 @@ public class IndexDatabase {
         Arrays.sort(files, FILENAME_COMPARATOR);
 
         for (File file : files) {
-            String path = parent + '/' + file.getName();
+            String path = parent + File.separator + file.getName();
             if (!accept(dir, file, outLocalRelPath)) {
                 if (outLocalRelPath[0] != null) {
                     File xrefPath = new File(xrefDir, path);
