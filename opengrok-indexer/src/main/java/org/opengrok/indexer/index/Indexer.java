8da27e96cfd9c73f4ce34b330156044bcfa5136e
Vladimir Kotal <vlada@devnull.cz>
2018-10-01 19:52:20 +0200

    remove unused import

--- opengrok-indexer/src/main/java/org/opengrok/indexer/index/Indexer.java
+++ opengrok-indexer/src/main/java/org/opengrok/indexer/index/Indexer.java
@@ -28,7 +28,6 @@ import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
-import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.file.Paths;
