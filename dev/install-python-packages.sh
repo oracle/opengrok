2a3a1daa2b6103551b2139e7ba63da7b5e19b1a4
Vladimir Kotal <vlada@devnull.cz>
2018-10-24 15:29:00 +0200

    check for errors

--- dev/install-python-packages.sh
+++ dev/install-python-packages.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
 python3 -m pip install --upgrade pip
+
 python3 -m pip install -r opengrok-tools/src/main/python/requirements.txt
+if [[ $? != 0 ]]; then
+	echo "cannot install Python requirement packages"
+	exit 1
+fi
+
 python3 -m pip install pep8 flake8 virtualenv pylint
+if [[ $? != 0 ]]; then
+	echo "cannot install Python packages"
+	exit 1
+fi
