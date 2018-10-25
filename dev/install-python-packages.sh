094edb8e8bf0dbce76031304bcb176e8c49945e0
Vladimir Kotal <vlada@devnull.cz>
2018-10-25 12:57:25 +0200

    Run pylint in venv (#2443)
    fixes #2442

--- dev/install-python-packages.sh
+++ dev/install-python-packages.sh
@@ -2,13 +2,7 @@
 
 python3 -m pip install --upgrade pip
 
-python3 -m pip install -r opengrok-tools/src/main/python/requirements.txt
-if [[ $? != 0 ]]; then
-	echo "cannot install Python requirement packages"
-	exit 1
-fi
-
-python3 -m pip install pep8 flake8 virtualenv pylint
+python3 -m pip install pep8 flake8 virtualenv
 if [[ $? != 0 ]]; then
 	echo "cannot install Python packages"
 	exit 1
