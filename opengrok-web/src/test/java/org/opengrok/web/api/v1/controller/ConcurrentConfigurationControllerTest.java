391c0e2260088f34f25567bf830575be1294020b
Kryštof Tulinger <k.tulinger@seznam.cz>
2018-11-11 10:40:13 +0100

    adding a test case for concurrent updates

--- opengrok-web/src/test/java/org/opengrok/web/api/v1/controller/ConcurrentConfigurationControllerTest.java
+++ opengrok-web/src/test/java/org/opengrok/web/api/v1/controller/ConcurrentConfigurationControllerTest.java
@@ -1,12 +1,16 @@
 package org.opengrok.web.api.v1.controller;
 
-import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.Response;
@@ -14,7 +18,9 @@ import org.apache.commons.io.FileUtils;
 import org.glassfish.jersey.internal.inject.AbstractBinder;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.test.JerseyTest;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -31,6 +37,10 @@ import org.opengrok.web.api.v1.suggester.provider.service.SuggesterService;
 
 public class ConcurrentConfigurationControllerTest extends JerseyTest {
 
+    private static final int PROJECTS_COUNT = 20;
+    private static final int THREAD_COUNT = Math.max(30, Runtime.getRuntime().availableProcessors() * 2);
+    private static final int TASK_COUNT = 100;
+
     @Rule
     public ConditionalRunRule rule = new ConditionalRunRule();
 
@@ -38,6 +48,12 @@ public class ConcurrentConfigurationControllerTest extends JerseyTest {
     private SuggesterService suggesterService;
 
     private RuntimeEnvironment env = RuntimeEnvironment.getInstance();
+
+    TestRepository repository;
+    String origSourceRootPath;
+    String origDataRootPath;
+    Map<String, Project> origProjects;
+    List<RepositoryInfo> origRepositories;
 
     @Override
     protected Application configure() {
@@ -51,33 +67,29 @@ public class ConcurrentConfigurationControllerTest extends JerseyTest {
                 });
     }
 
-    @Test
-    @ConditionalRun(RepositoryInstalled.GitInstalled.class)
-    public void testConcurrentConfigurationReloads() throws InterruptedException, IOException {
-        final String origSourceRootPath = env.getSourceRootPath();
-        final String origDataRootPath = env.getDataRootPath();
-        final Map<String, Project> origProjects = env.getProjects();
-        final List<RepositoryInfo> origRepositories = env.getRepositories();
-
-        final int nThreads = Math.max(40, Runtime.getRuntime().availableProcessors() * 2);
-        final int nProjects = 20;
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        origSourceRootPath = env.getSourceRootPath();
+        origDataRootPath = env.getDataRootPath();
+        origProjects = env.getProjects();
+        origRepositories = env.getRepositories();
 
         // prepare test repository
-        TestRepository repository = new TestRepository();
+        repository = new TestRepository();
         repository.create(HistoryGuru.class.getResourceAsStream("repositories.zip"));
 
         env.setSourceRoot(repository.getSourceRoot());
         env.setDataRoot(repository.getDataRoot());
 
-        final CountDownLatch latch = new CountDownLatch(nThreads);
 
         List<RepositoryInfo> repositoryInfos = new ArrayList<>();
         Map<String, Project> projects = new TreeMap<>();
 
         /*
-         * Prepare nProjects git repositories, named project-{i} in the test repositories directory.
+         * Prepare PROJECTS_COUNT git repositories, named project-{i} in the test repositories directory.
          */
-        for (int i = 0; i < nProjects; i++) {
+        for (int i = 0; i < PROJECTS_COUNT; i++) {
             Project project = new Project();
             project.setName("project-" + i);
             project.setPath("/project-" + i);
@@ -96,35 +108,81 @@ public class ConcurrentConfigurationControllerTest extends JerseyTest {
 
         env.setRepositories(repositoryInfos);
         env.setProjects(projects);
+    }
 
-        /*
-         * Now run setting a value in parallel, which triggers configuration reload.
-         */
-        for (int i = 0; i < nThreads; i++) {
-            new Thread(() -> {
-                Response put = target("configuration")
-                        .path("projectsEnabled")
-                        .request()
-                        .put(Entity.text("true"));
-                Assert.assertEquals(204, put.getStatus());
-                latch.countDown();
-            }).start();
-        }
-
-        latch.await();
-
-        Assert.assertEquals(nProjects, env.getProjects().size());
-        Assert.assertEquals(nProjects, env.getRepositories().size());
-        Assert.assertEquals(nProjects, env.getProjectRepositoriesMap().size());
-        env.getProjectRepositoriesMap().forEach((project, repositories) -> {
-            Assert.assertEquals(1, repositories.size());
-        });
-
+    @After
+    public void tearDown() throws Exception {
+        super.tearDown();
         repository.destroy();
 
         env.setProjects(origProjects);
         env.setRepositories(origRepositories);
         env.setSourceRoot(origSourceRootPath);
         env.setDataRoot(origDataRootPath);
+    }
+
+    @Test
+    @ConditionalRun(RepositoryInstalled.GitInstalled.class)
+    public void testConcurrentConfigurationReloads() throws InterruptedException, ExecutionException {
+        final ExecutorService threadPool = Executors.newFixedThreadPool(THREAD_COUNT);
+        List<Future<?>> futures = new LinkedList<>();
+
+        /*
+         * Now run setting a value in parallel which triggers configuration reload.
+         */
+        for (int i = 0; i < TASK_COUNT; i++) {
+            futures.add(threadPool.submit(() -> {
+                Response put = target("configuration")
+                        .path("projectsEnabled")
+                        .request()
+                        .put(Entity.text("true"));
+
+                Assert.assertEquals(204, put.getStatus());
+
+                assertTestedProjects();
+            }));
+        }
+
+        threadPool.shutdown();
+        threadPool.awaitTermination(100, TimeUnit.MINUTES);
+
+        for (Future<?> future : futures) {
+            future.get();
+        }
+    }
+
+    @Test
+    @ConditionalRun(RepositoryInstalled.GitInstalled.class)
+    public void testConcurrentCInvalidateRepositories() throws InterruptedException, ExecutionException {
+        final ExecutorService threadPool = Executors.newFixedThreadPool(THREAD_COUNT);
+        final List<RepositoryInfo> repositoryInfos = env.getRepositories();
+        List<Future<?>> futures = new LinkedList<>();
+
+        /*
+         * Now run apply config in parallel
+         */
+        for (int i = 0; i < TASK_COUNT; i++) {
+            futures.add(threadPool.submit(() -> {
+                env.applyConfig(false, false);
+
+                assertTestedProjects();
+            }));
+        }
+
+        threadPool.shutdown();
+        threadPool.awaitTermination(100, TimeUnit.MINUTES);
+
+        for (Future<?> future : futures) {
+            future.get();
+        }
+    }
+
+    private void assertTestedProjects() {
+        Assert.assertEquals(PROJECTS_COUNT, env.getProjects().size());
+        Assert.assertEquals(PROJECTS_COUNT, env.getRepositories().size());
+        Assert.assertEquals(PROJECTS_COUNT, env.getProjectRepositoriesMap().size());
+        env.getProjectRepositoriesMap().forEach((project, repositories) -> {
+            Assert.assertEquals(1, repositories.size());
+        });
     }
 }
