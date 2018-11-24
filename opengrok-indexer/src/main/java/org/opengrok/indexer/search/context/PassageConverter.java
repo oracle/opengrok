4d82b23a7ce69f19507184a1397a8c4b5de87e7d
Chris Fraire <cfraire@me.com>
2018-11-24 14:37:39 -0600

    Fix ContextFormatter and PassageConverter
    - Fix ContextFormatter bounds problem.
    - Fix PassageConverter mistakenly counting EOL and
      sometimes eliding too much from the left.
    - Simplify blocks.

--- opengrok-indexer/src/main/java/org/opengrok/indexer/search/context/PassageConverter.java
+++ opengrok-indexer/src/main/java/org/opengrok/indexer/search/context/PassageConverter.java
@@ -25,8 +25,10 @@ package org.opengrok.indexer.search.context;
 
 import java.util.SortedMap;
 import java.util.TreeMap;
+import java.util.regex.Matcher;
 import org.apache.lucene.search.uhighlight.Passage;
 import org.opengrok.indexer.util.SourceSplitter;
+import org.opengrok.indexer.util.StringUtils;
 
 /**
  * Represents an object that can translate {@link Passage} instances into
@@ -136,6 +138,10 @@ public class PassageConverter {
         for (LineHighlight lhi : res.values()) {
             lhi.condenseMarkups();
             String line = splitter.getLine(lhi.getLineno());
+            Matcher eolMatcher = StringUtils.STANDARD_EOL.matcher(line);
+            if (eolMatcher.find()) {
+                line = line.substring(0, eolMatcher.start());
+            }
             elideLine(lhi, line);
         }
 
@@ -148,32 +154,33 @@ public class PassageConverter {
             return;
         }
 
-        // Account for an ellipsis.
-        int nellip = 1;
-        ++excess;
-
         /*
          * The search/ view does not show leading whitespace anyway, so elide it
          * straight away.
          */
         int nwhsp0 = countStartingWhitespace(line);
         if (nwhsp0 > 0) {
-            lhi.setLelide(Math.min(nwhsp0, excess));
-            excess -= lhi.getLelide();
+            // Account for an ellipsis.
+            ++excess;
+            int leftAdj = Math.min(nwhsp0, excess);
+            lhi.setLelide(leftAdj);
+            excess -= leftAdj;
             if (excess <= 0) {
                 return;
             }
-
-            // Account for another ellipsis.
-            ++nellip;
-            ++excess;
         }
 
         int nwhspz = countEndingWhitespace(line);
-        // If the end of the line can be elided, just truncate it.
+        /*
+         * If the end of the line has enough whitespace to be elided (pre-
+         * accounting for another ellipsis), just truncate it.
+         */
         if (lhi.countMarkups() < 1 ||
                 lhi.getMarkup(lhi.countMarkups() - 1).getLineEnd() <
-                args.getContextWidth() || nwhspz >= excess) {
+                        args.getContextWidth() ||
+                nwhspz >= excess + 1) {
+            // Account for an ellipsis.
+            ++excess;
             lhi.setRelide(line.length() - excess);
             return;
         }
@@ -182,40 +189,27 @@ public class PassageConverter {
          * Find the width of bounds of markups.
          */
         int lbound = -1, rbound = -1;
-        for (int i = 0; i < lhi.countMarkups(); ++i) {
-            PhraseHighlight phi = lhi.getMarkup(i);
-            if (phi.getLineStart() >= 0) {
-                lbound = phi.getLineStart();
-                break;
-            } else if (phi.getLineStart() < 0) {
-                lbound = phi.getLineStart();
-                break;
-            } else if (phi.getLineEnd() != Integer.MAX_VALUE) {
-                lbound = phi.getLineEnd() - 1;
-                break;
-            } else if (phi.getLineEnd() == Integer.MAX_VALUE) {
+        if (lhi.countMarkups() > 0) {
+            PhraseHighlight phi = lhi.getMarkup(0);
+            lbound = phi.getLineStart();
+            if (lbound >= line.length()) {
                 lbound = line.length() - 1;
-                break;
             }
-        }
-        for (int i = lhi.countMarkups() - 1; i >= 0; --i) {
-            PhraseHighlight phi = lhi.getMarkup(i);
-            if (phi.getLineEnd() != Integer.MAX_VALUE) {
-                rbound = phi.getLineEnd();
-                break;
-            } else if (phi.getLineEnd() != Integer.MAX_VALUE) {
+
+            phi = lhi.getMarkup(lhi.countMarkups() - 1);
+            rbound = phi.getLineEnd();
+            if (rbound > line.length()) {
                 rbound = line.length();
-                break;
-            } else if (phi.getLineStart() >= 0) {
-                rbound = phi.getLineStart() + 1;
-                break;
-            } else if (phi.getLineStart() < 0) {
-                rbound = 1;
-                break;
             }
         }
-        // If the markup bounds are separated from the left margin...
-        if (lbound > 0 && rbound > 0) {
+
+        /*
+         * If the markup bounds are separated from the left margin, calculate
+         * elision bounds that contain as much of the highlighted area as
+         * possible, favoring the leftward highlights if the highlighted area
+         * exceeds the context-width.
+         */
+        if (lbound > 0 && rbound >= lbound) {
             /*
              * First use a rough estimate of three-quarters of a context-width
              * before the midpoint of lbound and rbound.
@@ -227,25 +221,29 @@ public class PassageConverter {
                 calcLeft = lbound;
             }
             if (calcLeft > lhi.getLelide()) {
-                // Possibly account for another ellipsis.
+                // Possibly account for an ellipsis.
                 if (lhi.getLelide() < 1) {
-                    ++nellip;
                     ++excess;
                 }
-                excess -= calcLeft - lhi.getLelide();
-                lhi.setLelide(calcLeft);
+                int leftAdj = Math.min(calcLeft - lhi.getLelide(), excess);
+                excess -= leftAdj;
+                lhi.setLelide(lhi.getLelide() + leftAdj);
             }
-            // Continue below.
         }
 
-        // Truncate the line finally.
-        lhi.setRelide(line.length() - excess);
-        if (nellip > 1) {
-            /**
-             * Possibly shift the left elide leftward in case the rough
+        // Possibly truncate the line finally.
+        if (excess > 0) {
+            // Account for another ellipsis.
+            ++excess;
+            lhi.setRelide(line.length() - excess);
+            /*
+             * Possibly shift the left elision leftward in case the rough
              * estimate above was too far rightward.
              */
-            lhi.setLelide(lhi.getRelide() - args.getContextWidth() + nellip);
+            if (lhi.getLelide() > 0) {
+                lhi.setLelide(lhi.getRelide() - args.getContextWidth() +
+                        2 /* two ellipses */);
+            }
         }
     }
 
