f4c4fd64489a9270fe933b495c2146d6aaa151ff
Vladimir Kotal <vlada@devnull.cz>
2019-10-03 16:53:59 +0200

    remove unused import

--- opengrok-indexer/src/main/java/org/opengrok/indexer/index/PendingFileCompleter.java
+++ opengrok-indexer/src/main/java/org/opengrok/indexer/index/PendingFileCompleter.java
@@ -42,7 +42,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
