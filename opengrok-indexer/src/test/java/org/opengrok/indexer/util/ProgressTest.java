c8016b7d23a649387ff0eb33902fbd10b37674ad
Vladimir Kotal <vlada@devnull.cz>
2019-10-09 16:16:13 +0200

    add test with threads

--- opengrok-indexer/src/test/java/org/opengrok/indexer/util/ProgressTest.java
+++ opengrok-indexer/src/test/java/org/opengrok/indexer/util/ProgressTest.java
@@ -1,38 +1,41 @@
 package org.opengrok.indexer.util;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.mockito.Mockito;
 import org.opengrok.indexer.configuration.RuntimeEnvironment;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
 import static org.junit.Assert.assertNotNull;
 import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.atLeast;
 import static org.mockito.Mockito.times;
 
 public class ProgressTest {
+    @BeforeClass
+    public static void setup() {
+        // needed to spawn logger thread in Progress
+        RuntimeEnvironment.getInstance().setPrintProgress(true);
+    }
+
     @Test
     public void testProgress() throws InterruptedException {
         final Logger logger = Mockito.mock(Logger.class);
         final int totalCount = 100; // use int because of Mockito.calls()/times() only accepts int
 
-        // needed to spawn logger thread in Progress
-        RuntimeEnvironment.getInstance().setPrintProgress(true);
-
         Mockito.when(logger.isLoggable(any())).thenReturn(true);
-        boolean first = true;
         try (final Progress progress = new Progress(logger, "foo", totalCount)) {
             assertNotNull(progress.getLoggerThread());
 
             // Progress does not give any indication that the logger thread reached the point when
             // it can accept notifications from increment() so check the Thread state.
-            if (first) {
-                while (progress.getLoggerThread().getState() != Thread.State.WAITING) {
-                    System.out.println("Waiting for the logger thread to reach the initial wait()");
-                    TimeUnit.MILLISECONDS.sleep(10);
-                }
-                first = false;
+            while (progress.getLoggerThread().getState() != Thread.State.WAITING) {
+                System.out.println("Waiting for the logger thread to reach the initial wait()");
+                TimeUnit.MILLISECONDS.sleep(10);
             }
 
             for (int i = 0; i < totalCount; i++) {
@@ -45,5 +48,22 @@ public class ProgressTest {
         }
 
         Mockito.verify(logger, times(totalCount)).log(any(), anyString(), any(Object[].class));
+    }
+
+    @Test
+    public void testThreads() throws InterruptedException {
+        final Logger logger = Mockito.mock(Logger.class);
+        final int totalCount = Runtime.getRuntime().availableProcessors();
+
+        Mockito.when(logger.isLoggable(any())).thenReturn(true);
+        ExecutorService executor = Executors.newFixedThreadPool(totalCount);
+        try (final Progress progress = new Progress(logger, "foo", totalCount)) {
+            for (int i = 0; i < totalCount; i++) {
+                executor.submit(progress::increment);
+            }
+        }
+
+        executor.awaitTermination(10, TimeUnit.SECONDS);
+        Mockito.verify(logger, atLeast(1)).log(any(), anyString(), any(Object[].class));
     }
 }
