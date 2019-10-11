ba76c2f054d8202c3657136271cb791c07bd108d
Vladimir Kotal <vlada@devnull.cz>
2019-10-11 22:12:19 +0200

    address some review comments

--- opengrok-indexer/src/test/java/org/opengrok/indexer/util/ProgressTest.java
+++ opengrok-indexer/src/test/java/org/opengrok/indexer/util/ProgressTest.java
@@ -31,14 +31,16 @@ public class ProgressTest {
     public void testProgress() throws InterruptedException {
         final Logger logger = Mockito.mock(Logger.class);
         final int totalCount = 100; // use int because of Mockito.calls()/times() only accepts int
+        Thread loggerThread = null;
 
         Mockito.when(logger.isLoggable(any())).thenReturn(true);
         try (final Progress progress = new Progress(logger, "foo", totalCount)) {
             assertNotNull(progress.getLoggerThread());
+            loggerThread = progress.getLoggerThread();
 
             // Progress does not give any indication that the logger thread reached the point when
             // it can accept notifications from increment() so check the Thread state.
-            while (progress.getLoggerThread().getState() != Thread.State.WAITING) {
+            while (loggerThread.getState() != Thread.State.WAITING) {
                 System.out.println("Waiting for the logger thread to reach the initial wait()");
                 TimeUnit.MILLISECONDS.sleep(10);
             }
@@ -46,12 +48,13 @@ public class ProgressTest {
             for (int i = 0; i < totalCount; i++) {
                 progress.increment();
                 // Give the logger thread some time to log.
-                while (progress.getLoggerThread().getState() != Thread.State.WAITING) {
+                while (loggerThread.getState() != Thread.State.WAITING) {
                     TimeUnit.MILLISECONDS.sleep(10);
                 }
             }
         }
 
+        Assert.assertSame(loggerThread.getState(), Thread.State.TERMINATED);
         Mockito.verify(logger, times(totalCount)).log(any(), anyString(), any(Object[].class));
     }
 
@@ -73,16 +76,17 @@ public class ProgressTest {
             for (int i = 0; i < totalCount; i++) {
                 futures.add(executor.submit(progress::increment));
             }
+
+            System.out.println("Waiting for threads to finish");
+            futures.forEach(future -> {
+                try {
+                    future.get();
+                } catch (Exception e) {
+                    Assert.fail();
+                }
+            });
         }
 
-        System.out.println("Waiting for threads to finish");
-        futures.forEach(future -> {
-            try {
-                future.get();
-            } catch (Exception e) {
-                Assert.fail();
-            }
-        });
         executor.shutdown();
         System.out.println("Verifying");
         Mockito.verify(logger, atLeast(1)).log(any(), anyString(), any(Object[].class));
