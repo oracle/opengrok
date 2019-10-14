555fbbc81ac68966c8c9b16e129fd50ba7548faf
Vladimir Kotal <vlada@devnull.cz>
2019-10-14 14:15:53 +0200

    LDAP uid case (in)sensitivity fix for LdapFilter plugin (#2949)
    fixes #2946

--- plugins/src/main/java/opengrok/auth/plugin/util/FilterUtil.java
+++ plugins/src/main/java/opengrok/auth/plugin/util/FilterUtil.java
@@ -25,10 +25,74 @@ package opengrok.auth.plugin.util;
 
 import opengrok.auth.plugin.entity.User;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
+
 public class FilterUtil {
 
     private FilterUtil() {
         // utility class
+    }
+
+    private static final String LOWER_CASE = "toLowerCase";
+    private static final String UPPER_CASE = "toUpperCase";
+
+    /**
+     * Verify the names of the transforms in the map.
+     * @param transforms map of attribute transforms. Valid values: <code>toLowerCase, toUpperCase</code>
+     * @throws UnsupportedOperationException in case of invalid transform name
+     */
+    public static void checkTransforms(Map<String, String> transforms) {
+        Set<String> possibleTransforms = new HashSet<>(Arrays.asList(LOWER_CASE, UPPER_CASE));
+        for (String transform : transforms.values()) {
+            if (!possibleTransforms.contains(transform)) {
+                throw new UnsupportedOperationException(String.format("invalid transform: %s", transform));
+            }
+        }
+    }
+
+    static String doTransform(String value, String transform) {
+        switch (transform) {
+            case LOWER_CASE:
+                return value.toLowerCase(Locale.ROOT);
+            case UPPER_CASE:
+                return value.toUpperCase(Locale.ROOT);
+            default:
+                throw new UnsupportedOperationException(String.format("transform '%s' is unsupported", transform));
+        }
+    }
+
+    /**
+     * Expand attributes in filter string.
+     * @param filter input string
+     * @param name attribute name
+     * @param value value to replace
+     * @param transforms map of transformations to be potentially applied on the value
+     * @return new value of the string
+     */
+    public static String replace(String filter, String name, String value, Map<String, String> transforms) {
+        if (transforms != null) {
+            String transform;
+            if ((transform = transforms.get(name)) != null) {
+                value = doTransform(value, transform);
+            }
+        }
+
+        return filter.replaceAll("(?<!\\\\)%" + name + "(?<!\\\\)%", value);
+    }
+
+    /**
+     * Replace attribute names with values in filter string.
+     * @param user User object
+     * @param filter filter string
+     * @return filter with the values replaced
+     * @see #expandUserFilter(User, String, Map)
+     */
+    public static String expandUserFilter(User user, String filter) {
+        return expandUserFilter(user, filter, null);
     }
 
     /**
@@ -41,14 +105,16 @@ public class FilterUtil {
      * </ul>
      *
      * @param user User object from the request (created by {@code UserPlugin})
+     * @param filter filter
+     * @param transforms map of transforms
      * @return replaced result
      */
-    public static String expandUserFilter(User user, String filter) {
+    public static String expandUserFilter(User user, String filter, Map<String, String> transforms) {
         if (user.getUsername() != null) {
-            filter = filter.replaceAll("(?<!\\\\)%username(?<!\\\\)%", user.getUsername());
+            filter = replace(filter, "username", user.getUsername(), transforms);
         }
         if (user.getId() != null) {
-            filter = filter.replaceAll("(?<!\\\\)%guid(?<!\\\\)%", user.getId());
+            filter = replace(filter, "guid", user.getId(), transforms);
         }
 
         return filter;
---
+++ plugins/src/test/java/opengrok/auth/plugin/util/FilterUtilTest.java
@@ -0,0 +1,69 @@
+/*
+ * CDDL HEADER START
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
+ *
+ * See LICENSE.txt included in this distribution for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at LICENSE.txt.
+ * If applicable, add the following below this CDDL HEADER, with the
+ * fields enclosed by brackets "[]" replaced with your own identifying
+ * information: Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ */
+
+/*
+ * Copyright (c) 2019 Oracle and/or its affiliates. All rights reserved.
+ */
+
+package opengrok.auth.plugin.util;
+
+import org.junit.Test;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Map;
+import java.util.TreeMap;
+
+import static opengrok.auth.plugin.util.FilterUtil.doTransform;
+import static org.junit.Assert.assertEquals;
+
+public class FilterUtilTest {
+    @Test
+    public void testTransforms() {
+        assertEquals("FOO", doTransform("foo", "toUpperCase"));
+        assertEquals("foo", doTransform("FOO", "toLowerCase"));
+    }
+
+    @Test
+    public void testTransformsUTF() throws UnsupportedEncodingException {
+        assertEquals(new String("ČUČKAŘ".getBytes("UTF-8"), "UTF-8"),
+                doTransform(new String("čučkař".getBytes("UTF-8"), "UTF-8"), "toUpperCase"));
+        assertEquals(new String("čučkař".getBytes("UTF-8"), "UTF-8"),
+                doTransform(new String("ČUČKAŘ".getBytes("UTF-8"), "UTF-8"), "toLowerCase"));
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void testInvalidTransform() {
+        doTransform("foo", "bar");
+    }
+
+    @Test
+    public void testReplace() {
+        Map<String, String> transforms = new TreeMap<>();
+        transforms.put("uid", "toUpperCase");
+        assertEquals("fooUSERbar",
+                FilterUtil.replace("foo%uid%bar", "uid", "user", transforms));
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void testCheckTransforms() {
+        Map<String, String> transforms = new TreeMap<>();
+        transforms.put("uid", "xxx");
+        FilterUtil.checkTransforms(transforms);
+    }
+}
