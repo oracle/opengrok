b97c1aa2e385fcaa5e252894f2b4a21a4b57bd17
Vladimir Kotal <vlada@devnull.cz>
2019-10-25 13:38:42 +0200

    removed unused import

--- opengrok-indexer/src/main/java/org/opengrok/indexer/web/PageConfig.java
+++ opengrok-indexer/src/main/java/org/opengrok/indexer/web/PageConfig.java
@@ -51,7 +51,6 @@ import java.util.SortedSet;
 import java.util.TreeSet;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.regex.Pattern;
