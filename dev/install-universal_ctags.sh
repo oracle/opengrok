cbc9e7e9ca99a6c8da1c5f288ea55ac06df47a13
Vladimir Kotal <vlada@devnull.cz>
2019-11-11 16:09:05 +0100

    reset to last good tag (1295de210df49c979f27e185106a7ebc55146c57)
    provides relief for #2977

--- dev/install-universal_ctags.sh
+++ dev/install-universal_ctags.sh
@@ -2,6 +2,8 @@
 
 git clone https://github.com/universal-ctags/ctags.git
 cd ctags
+# need to use last working version until issue #2977 is resolved.
+git reset --hard 1295de210df49c979f27e185106a7ebc55146c57
 ./autogen.sh
 ./configure && make && make install
 /usr/local/bin/ctags --version
