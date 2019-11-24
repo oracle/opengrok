e0c3b7e91dd46566c4eaba57043275738c7a5e75
Vladimir Kotal <vlada@devnull.cz>
2019-11-24 19:46:31 +0100

    fix comment

--- plugins/src/main/java/opengrok/auth/plugin/util/RestfulClient.java
+++ plugins/src/main/java/opengrok/auth/plugin/util/RestfulClient.java
@@ -33,7 +33,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- * Simple RESTful client code for PUT requests.
+ * Simple RESTful client.
  */
 public class RestfulClient {
     private static final Logger LOGGER = Logger.getLogger(RestfulClient.class.getName());
