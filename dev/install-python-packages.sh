9d51914203bc23757bacbf1afcffe6a28075ffa7
Vladimir Kotal <vlada@devnull.cz>
2020-02-14 15:20:59 +0100

    use sudo -H when installing Python packages

--- dev/install-python-packages.sh
+++ dev/install-python-packages.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+echo "Installing/upgrading pip.."
 python3 -m pip install --upgrade pip
 
+echo "Installing Python packages.."
 python3 -m pip install pep8 virtualenv
 if [[ $? != 0 ]]; then
 	echo "cannot install Python packages"
