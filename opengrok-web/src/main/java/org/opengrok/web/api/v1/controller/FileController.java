f41f611c79482c4c48cef499b6e264f2c4de7762
Vladimir Kotal <vlada@devnull.cz>
2020-02-25 19:32:31 +0100

    remove unused import

--- opengrok-web/src/main/java/org/opengrok/web/api/v1/controller/FileController.java
+++ opengrok-web/src/main/java/org/opengrok/web/api/v1/controller/FileController.java
@@ -48,7 +48,6 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
