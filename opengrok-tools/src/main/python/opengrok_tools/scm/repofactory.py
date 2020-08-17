f1d1f5c48c45a86486683dbabbc70067ae43b6af
bobdoah <robertwilliams1985@gmail.com>
2020-08-17 10:29:21 +0100

    Add mirroring support for Perforce repositories (#3185)

--- opengrok-tools/src/main/python/opengrok_tools/scm/repofactory.py
+++ opengrok-tools/src/main/python/opengrok_tools/scm/repofactory.py
@@ -20,12 +20,14 @@
 #
 # Copyright (c) 2018, 2020, Oracle and/or its affiliates. All rights reserved.
 #
+# Portions Copyright 2020 Robert Williams
 
 import logging
 
 from .cvs import CVSRepository
 from .git import GitRepository
 from .mercurial import MercurialRepository
+from .perforce import PerforceRepository
 from .repo import RepoRepository
 from .svn import SubversionRepository
 from .teamware import TeamwareRepository
@@ -74,6 +76,10 @@ def get_repository(path, repo_type, project,
         return GitRepository(logger, path, project,
                              commands.get("git"),
                              env, hooks, timeout)
+    elif repo_lower == "perforce":
+        return PerforceRepository(logger, path, project,
+                                  commands.get("perforce"),
+                                  env, hooks, timeout)
     elif repo_lower == "repo":
         return RepoRepository(logger, path, project,
                               commands.get("repo"),
