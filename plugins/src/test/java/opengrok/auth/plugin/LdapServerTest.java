d7c7f8a598fa9dba093a5ac5d15c61db68f84aa5
Vladimir Kotal <vlada@devnull.cz>
2020-08-28 15:15:52 +0200

    remove unused import

--- plugins/src/test/java/opengrok/auth/plugin/LdapServerTest.java
+++ plugins/src/test/java/opengrok/auth/plugin/LdapServerTest.java
@@ -14,7 +14,6 @@ import java.util.concurrent.CountDownLatch;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doReturn;
 
 public class LdapServerTest {
