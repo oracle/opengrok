8bfce17fca9f8e991992d6491caa45b51c625cdd
Vladimir Kotal <vlada@devnull.cz>
2020-10-15 12:20:30 +0200

    use statsd for indexer monitoring (#3279)
    introduce StatsdRegistry
    
    Co-authored-by: Adam Hornáček <adam.hornacek@icloud.com>
    
    fixes #3245

--- opengrok-indexer/src/main/java/org/opengrok/indexer/Metrics.java
+++ opengrok-indexer/src/main/java/org/opengrok/indexer/Metrics.java
@@ -22,6 +22,9 @@
  */
 package org.opengrok.indexer;
 
+import io.micrometer.core.instrument.Clock;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tag;
 import io.micrometer.core.instrument.binder.jvm.ClassLoaderMetrics;
 import io.micrometer.core.instrument.binder.jvm.JvmGcMetrics;
 import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics;
@@ -29,24 +32,111 @@ import io.micrometer.core.instrument.binder.jvm.JvmThreadMetrics;
 import io.micrometer.core.instrument.binder.system.ProcessorMetrics;
 import io.micrometer.prometheus.PrometheusConfig;
 import io.micrometer.prometheus.PrometheusMeterRegistry;
+import io.micrometer.statsd.StatsdConfig;
+import io.micrometer.statsd.StatsdMeterRegistry;
+import io.micrometer.statsd.StatsdFlavor;
+import org.opengrok.indexer.configuration.RuntimeEnvironment;
+import org.opengrok.indexer.index.Indexer;
+import org.opengrok.indexer.logger.LoggerFactory;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+
+/**
+ * Encapsulates logic of meter registry setup and handling.
+ * Generally, the web application publishes metrics to Prometheus and the Indexer to StatsD.
+ */
 public final class Metrics {
 
-    private static final PrometheusMeterRegistry registry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
+    private static final Logger LOGGER = LoggerFactory.getLogger(Metrics.class);
+
+    private static final StatsdConfig statsdConfig = new StatsdConfig() {
+        @Override
+        public String get(String k) {
+            return null;
+        }
+
+        @Override
+        public StatsdFlavor flavor() {
+            return RuntimeEnvironment.getInstance().getStatsdConfig().getFlavor();
+        }
+
+        @Override
+        public int port() {
+            return RuntimeEnvironment.getInstance().getStatsdConfig().getPort();
+        }
+
+        @Override
+        public String host() {
+            return RuntimeEnvironment.getInstance().getStatsdConfig().getHost();
+        }
+
+        @Override
+        public boolean buffered() {
+            return true;
+        }
+    };
+
+    private static PrometheusMeterRegistry prometheusRegistry;
+    private static StatsdMeterRegistry statsdRegistry;
 
     static {
-        new ClassLoaderMetrics().bindTo(registry);
-        new JvmMemoryMetrics().bindTo(registry);
-        new JvmGcMetrics().bindTo(registry);
-        new ProcessorMetrics().bindTo(registry);
-        new JvmThreadMetrics().bindTo(registry);
+        MeterRegistry registry = null;
+
+        if (RuntimeEnvironment.getInstance().getStatsdConfig().isEnabled()) {
+            LOGGER.log(Level.INFO, "configuring StatsdRegistry");
+            statsdRegistry = new StatsdMeterRegistry(statsdConfig, Clock.SYSTEM);
+            registry = statsdRegistry;
+        } else if (!RuntimeEnvironment.getInstance().isIndexer()) {
+            LOGGER.log(Level.INFO, "configuring PrometheusRegistry");
+            prometheusRegistry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
+            registry = prometheusRegistry;
+        }
+
+        if (registry != null) {
+            new ClassLoaderMetrics().bindTo(registry);
+            new JvmMemoryMetrics().bindTo(registry);
+            new JvmGcMetrics().bindTo(registry);
+            new ProcessorMetrics().bindTo(registry);
+            new JvmThreadMetrics().bindTo(registry);
+        }
     }
 
     private Metrics() {
     }
 
-    public static PrometheusMeterRegistry getRegistry() {
-        return registry;
+    public static void updateSubFiles(List<String> subFiles) {
+        // Add tag for per-project reindex.
+        if (statsdRegistry != null && !subFiles.isEmpty()) {
+            String projects = subFiles.stream().
+                    map(s -> s.startsWith(Indexer.PATH_SEPARATOR_STRING) ? s.substring(1) : s).
+                    collect(Collectors.joining(","));
+            Tag commonTag = Tag.of("projects", projects);
+            LOGGER.log(Level.FINE, "updating statsdRegistry with common tag: {}", commonTag);
+            statsdRegistry.config().commonTags(Collections.singleton(commonTag));
+        }
     }
 
+    public static PrometheusMeterRegistry getPrometheusRegistry() {
+        return prometheusRegistry;
+    }
+
+    private static StatsdMeterRegistry getStatsdRegistry() {
+        return statsdRegistry;
+    }
+
+    /**
+     * Get registry based on running context.
+     * @return MeterRegistry instance
+     */
+    public static MeterRegistry getRegistry() {
+        if (RuntimeEnvironment.getInstance().isIndexer()) {
+            return getStatsdRegistry();
+        } else {
+            return getPrometheusRegistry();
+        }
+    }
 }
