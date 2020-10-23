814ddb49ae9655a92ae2b97f466ab9f5fe4001fb
Vladimir Kotal <vlada@devnull.cz>
2020-10-23 14:24:42 +0200

    initial stab at token authentication
    fixes #3222

--- opengrok-indexer/src/main/java/org/opengrok/indexer/configuration/RuntimeEnvironment.java
+++ opengrok-indexer/src/main/java/org/opengrok/indexer/configuration/RuntimeEnvironment.java
@@ -1935,4 +1935,8 @@ public final class RuntimeEnvironment {
     private int getMessageLimit() {
         return syncReadConfiguration(Configuration::getMessageLimit);
     }
+
+    public Set<String> getTokens() {
+        return new HashSet<>(Arrays.asList("foo", "bar"));
+    }
 }
