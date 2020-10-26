ced24af6f459b5a82ab315b95f560b3a507e3355
Krystof Tulinger <k.tulinger@seznam.cz>
2020-10-27 00:53:18 +0100

    using factory for system binaries

--- opengrok-tools/src/test/python/conftest.py
+++ opengrok-tools/src/test/python/conftest.py
@@ -1,42 +1,18 @@
 import os
+
 import pytest
 
 
-@pytest.fixture(
-    params=[
-        pytest.param('/bin/true', marks=pytest.mark.skipif(not os.path.exists('/bin/true'), reason="requires /bin binaries")),
-        pytest.param('/usr/bin/true', marks=pytest.mark.skipif(not os.path.exists('/usr/bin/true'), reason="requires /usr/bin binaries"))
-    ]
-)
-def true_binary(request):
-    return request.param
+def system_binary(name):
+    def decorator(fn):
+        return pytest.mark.parametrize(
+            ('{}_binary'.format(name)), [
+                pytest.param('/bin/{}'.format(name), marks=pytest.mark.skipif(not os.path.exists('/bin/{}'.format(name)), reason="requires /bin binaries")),
+                pytest.param('/usr/bin/{}'.format(name), marks=pytest.mark.skipif(not os.path.exists('/usr/bin/{}'.format(name)), reason="requires /usr/bin binaries")),
+            ])(fn)
+
+    return decorator
 
 
-@pytest.fixture(
-    params=[
-        pytest.param('/bin/false', marks=pytest.mark.skipif(not os.path.exists('/bin/false'), reason="requires /bin binaries")),
-        pytest.param('/usr/bin/false', marks=pytest.mark.skipif(not os.path.exists('/usr/bin/false'), reason="requires /usr/bin binaries"))
-    ]
-)
-def false_binary(request):
-    return request.param
-
-
-@pytest.fixture(
-    params=[
-        pytest.param('/bin/touch', marks=pytest.mark.skipif(not os.path.exists('/bin/touch'), reason="requires /bin binaries")),
-        pytest.param('/usr/bin/touch', marks=pytest.mark.skipif(not os.path.exists('/usr/bin/touch'), reason="requires /usr/bin binaries"))
-    ]
-)
-def touch_binary(request):
-    return request.param
-
-
-@pytest.fixture(
-    params=[
-        pytest.param('/bin/echo', marks=pytest.mark.skipif(not os.path.exists('/bin/echo'), reason="requires /bin binaries")),
-        pytest.param('/usr/bin/echo', marks=pytest.mark.skipif(not os.path.exists('/usr/bin/echo'), reason="requires /usr/bin binaries"))
-    ]
-)
-def echo_binary(request):
-    return request.param
+def posix_only(fn):
+    return pytest.mark.skipif(not os.name.startswith("posix"), reason="requires posix")(fn)
