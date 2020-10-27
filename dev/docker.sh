fcb1dedcc30798ea969670e5c0c00ee7dad6ed03
Vladimir Kotal <vlada@devnull.cz>
2020-10-27 19:42:39 +0100

    be silent

--- dev/docker.sh
+++ dev/docker.sh
@@ -131,4 +131,4 @@ fi
 push_readme "${IMAGE}" "${TOKEN}" "docker/README.md"
 
 # update Microbadger
-curl -X POST https://hooks.microbadger.com/images/opengrok/docker/pSastb42Ikfn2dF5llR54sSPqbQ=
+curl -s -X POST https://hooks.microbadger.com/images/opengrok/docker/pSastb42Ikfn2dF5llR54sSPqbQ=
