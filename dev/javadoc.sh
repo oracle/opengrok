9a176f331a5b9086fbe5ff3ad90114e63ccd56fa
Vladimir Kotal <vlada@devnull.cz>
2020-10-27 16:26:46 +0100

    use variable for branch name

--- dev/javadoc.sh
+++ dev/javadoc.sh
@@ -10,22 +10,23 @@ if [[ "${TRAVIS_REPO_SLUG}" != "oracle/opengrok" ||
 	exit 0
 fi
 
-echo -e "Publishing javadoc...\n"
+BRANCH="gh-pages"
+echo -e "Publishing javadoc to $BRANCH...\n"
 
 ./mvnw -DskipTests=true site
 
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "travis-ci"
-git clone --quiet --branch=gh-pages \
-    https://${GH_PAGES_TOKEN}@github.com/oracle/opengrok gh-pages
+git clone --quiet --branch=$BRANCH \
+    https://${GH_PAGES_TOKEN}@github.com/oracle/opengrok "$BRANCH"
 
-cd gh-pages
+cd "$BRANCH"
 if [[ -d ./javadoc ]]; then
 	git rm -rf ./javadoc
 fi
 cp -Rf ${TRAVIS_BUILD_DIR}/target/site/apidocs ./javadoc
 git add -f ./javadoc
-git commit -m "Lastest javadoc auto-pushed to gh-pages"
-git push -fq origin gh-pages
+git commit -m "Lastest javadoc auto-pushed to branch $BRANCH"
+git push -fq origin "$BRANCH"
 
-echo -e "Published Javadoc to gh-pages.\n"
+echo -e "Published Javadoc to branch $BRANCH.\n"
