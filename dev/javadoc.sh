45e0b9056f098a348e99521f314569235b4d52fa
Vladimir Kotal <vlada@devnull.cz>
2020-10-29 18:58:53 +0100

    use checkout action

--- dev/javadoc.sh
+++ dev/javadoc.sh
@@ -3,6 +3,11 @@
 set -e
 set -x
 
+if [[ -z "$OPENGROK_BUILD_DIR" ]]; then
+	echo -e "empty OPENGROK_BUILD_DIR"
+	exit 1
+fi
+
 if [[ -n $OPENGROK_REF && $OPENGROK_REF == refs/heads/* ]]; then
 	OPENGROK_BRANCH=${OPENGROK_REF#"refs/heads/"}
 fi
@@ -22,14 +27,12 @@ echo -e "Building Javadoc...\n"
 echo -e "Publishing javadoc to $BRANCH...\n"
 git config --global user.name "github-actions[bot]"
 git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"
-git clone --quiet --branch=$BRANCH \
-    https://github.com/oracle/opengrok "$BRANCH"
 
 cd "$BRANCH"
 if [[ -d ./javadoc ]]; then
 	git rm -rf ./javadoc
 fi
-cp -Rf ${OPENGROK_BUILD_DIR}/target/site/apidocs ./javadoc
+cp -Rf "$OPENGROK_BUILD_DIR/target/site/apidocs" ./javadoc
 git add -f ./javadoc
 git commit -m "Lastest javadoc auto-pushed to branch $BRANCH"
 git push -fq origin "$BRANCH"
