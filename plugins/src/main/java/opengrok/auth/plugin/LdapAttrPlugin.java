25523f4f359a28206aceb47a0d734dfab6562747
Vladimir Kotal <vlada@devnull.cz>
2020-11-03 16:09:46 +0100

    log more info during init() in plugins using whitelist

--- plugins/src/main/java/opengrok/auth/plugin/LdapAttrPlugin.java
+++ plugins/src/main/java/opengrok/auth/plugin/LdapAttrPlugin.java
@@ -110,8 +110,8 @@ public class LdapAttrPlugin extends AbstractLdapPlugin {
             throw new IllegalArgumentException(String.format("Unable to read the file \"%s\"", filePath), e);
         }
 
-        LOGGER.log(Level.FINE, "LdapAttrPlugin plugin loaded with attr={0}, whitelist={1}, instance={2}",
-                new Object[]{ldapAttr, filePath, ldapUserInstance});
+        LOGGER.log(Level.FINE, "LdapAttrPlugin plugin loaded with attr={0}, whitelist={1} ({2} entries), " +
+                        "instance={3}", new Object[]{ldapAttr, filePath, whitelist.size(), ldapUserInstance});
     }
 
     @Override
