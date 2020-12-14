53d7bb69ffe487568ed2659cce1321ea290081bd
Vladimir Kotal <vlada@devnull.cz>
2020-12-14 10:17:08 +0100

    refactor to avoid special casing

--- docker/start.sh
+++ docker/start.sh
@@ -20,58 +20,102 @@ else
 	WAR_NAME="${URL_ROOT//\//#}.war"
 fi
 
-if [ ! -f "/usr/local/tomcat/webapps/${WAR_NAME}" ]; then
-	date +"%F %T Deployment path changed. Redeploying..."
+URI="http://localhost:8080/${URL_ROOT}"
+OPS=${INDEXER_FLAGS:='-H -P -S -G'}
+BODY_INCLUDE_FILE="/opengrok/data/body_include"
 
-	# Delete old deployment and (re)deploy
-	rm -rf /usr/local/tomcat/webapps/*
-	opengrok-deploy -c /opengrok/etc/configuration.xml \
-            /opengrok/lib/source.war "/usr/local/tomcat/webapps/${WAR_NAME}"
+function deploy {
+	if [[ ! -f "/usr/local/tomcat/webapps/${WAR_NAME}" ]]; then
+		date +"%F %T Deployment path does not exist. Deploying..."
 
-	# Set up redirect from /source
-	mkdir "/usr/local/tomcat/webapps/source"
-	echo "<% response.sendRedirect(\"/${URL_ROOT}\"); %>" > "/usr/local/tomcat/webapps/source/index.jsp"
-fi
+		# Delete old deployment and (re)deploy.
+		rm -rf /usr/local/tomcat/webapps/*
+		opengrok-deploy -c /opengrok/etc/configuration.xml \
+	            /opengrok/lib/source.war "/usr/local/tomcat/webapps/${WAR_NAME}"
 
-indexer(){
+		# Set up redirect from /source
+		mkdir "/usr/local/tomcat/webapps/source"
+		echo "<% response.sendRedirect(\"/${URL_ROOT}\"); %>" > "/usr/local/tomcat/webapps/source/index.jsp"
+	fi
+}
+
+function bare_config {
+	# Populate the webapp with bare configuration.
+	if [[ -f "$BODY_INCLUDE_FILE" ]]; then
+		mv "$BODY_INCLUDE_FILE" "$BODY_INCLUDE_FILE.orig"
+	fi
+	echo '<p><h1>Waiting on the initial reindex to finish.. Stay tuned !</h1></p>' > "$BODY_INCLUDE_FILE"
+
+	opengrok-indexer \
+	    -a /opengrok/lib/opengrok.jar -- \
+	    -s /opengrok/src \
+	    -d /opengrok/data \
+	    --remote on \
+	    -W /opengrok/etc/configuration.xml \
+	    --noIndex
+}
+
+function wait_for_tomcat {
 	# Wait for Tomcat startup.
 	date +"%F %T Waiting for Tomcat startup..."
 	while [ "`curl --silent --write-out '%{response_code}' -o /dev/null \"http://localhost:8080/${URL_ROOT}\"`" == "000" ]; do
 		sleep 1;
 	done
-	date +"%F %T Startup finished"
+	date +"%F %T Tomcat startup finished"
+}
 
-	if [[ ! -d /opengrok/data/index ]]; then
-		# Populate the webapp with bare configuration.
-		BODY_INCLUDE_FILE="/opengrok/data/body_include"
-		if [[ -f $BODY_INCLUDE_FILE ]]; then
-			mv "$BODY_INCLUDE_FILE" "$BODY_INCLUDE_FILE.orig"
-		fi
-		echo '<p><h1>Waiting on the initial reindex to finish.. Stay tuned !</h1></p>' > "$BODY_INCLUDE_FILE"
-		/scripts/index.sh --noIndex
+function data_sync {
+	if [ -z $NOMIRROR ]; then
+		date +"%F %T Mirroring starting"
+		opengrok-mirror --all --uri "$URI"
+		date +"%F %T Mirroring finished"
+	fi
+
+	date +"%F %T Indexing starting"
+	opengrok-indexer \
+	    -a /opengrok/lib/opengrok.jar -- \
+	    -s /opengrok/src \
+	    -d /opengrok/data \
+	    --remote on \
+	    -W /opengrok/etc/configuration.xml \
+	    -U "$URI" \
+	    $OPS \
+	    $INDEXER_OPT "$@"
+	date +"%F %T Indexing finished"
+}
+
+function indexer {
+	wait_for_tomcat
+
+	while true; do
+		data_sync
+
+		# If this was a case of initial indexing, move the include away.
 		rm -f "$BODY_INCLUDE_FILE"
 		if [[ -f $BODY_INCLUDE_FILE.orig ]]; then
 			mv "$BODY_INCLUDE_FILE.orig" "$BODY_INCLUDE_FILE"
 		fi
 
-		# Perform initial indexing.
-		NOMIRROR=1 /scripts/index.sh
-		date +"%F %T Initial reindex finished"
-	fi
-
-	# Continue to index every $REINDEX minutes.
-	if [ "$REINDEX" == "0" ]; then
-		date +"%F %T Automatic reindexing disabled"
-		return
-	else
-		date +"%F %T Automatic reindexing in $REINDEX minutes..."
-	fi
-	while true; do
+		# Index every $REINDEX minutes.
+		if [ "$REINDEX" == "0" ]; then
+			date +"%F %T Automatic reindexing disabled"
+			return
+		else
+			date +"%F %T Automatic reindexing in $REINDEX minutes..."
+		fi
 		sleep `expr 60 \* $REINDEX`
-		/scripts/index.sh
 	done
 }
 
-# Start all necessary services.
+deploy
+
+#
+# Create empty configuration to avoid the non existent file exception
+# during the first web app startup.
+#
+if [[ ! -f /opengrok/etc/configuration.xml ]]; then
+	bare_config
+fi
+
 indexer &
 catalina.sh run
