ba5494c87667e21da67d75be7a0000b8c0fa83ed
Vladimir Kotal <vlada@devnull.cz>
2021-03-08 13:25:34 +0100

    another missing type

--- tools/src/main/python/opengrok_tools/mirror.py
+++ tools/src/main/python/opengrok_tools/mirror.py
@@ -106,7 +106,7 @@ def main():
     parser.add_argument('-w', '--workers', default=cpu_count(),
                         help='Number of worker processes')
     add_http_headers(parser)
-    parser.add_argument('--api_timeout', proxies=None,
+    parser.add_argument('--api_timeout', type=int,
                         help='Set response timeout in seconds for RESTful API calls')
 
     try:
