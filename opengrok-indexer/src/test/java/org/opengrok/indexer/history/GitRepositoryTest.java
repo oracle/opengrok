f2975ae3e90ed396470343079be34bc0b6b68d99
Vladimir Kotal <vlada@devnull.cz>
2021-03-19 21:33:52 +0100

    remove unused import

--- opengrok-indexer/src/test/java/org/opengrok/indexer/history/GitRepositoryTest.java
+++ opengrok-indexer/src/test/java/org/opengrok/indexer/history/GitRepositoryTest.java
@@ -28,7 +28,6 @@ import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.text.ParseException;
 import java.util.Arrays;
