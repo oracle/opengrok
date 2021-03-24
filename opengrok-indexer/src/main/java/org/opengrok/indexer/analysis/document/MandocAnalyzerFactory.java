cd560f86765fc583114228f0328557c69215905d
Vladimir Kotal <vlada@devnull.cz>
2021-03-24 17:28:58 +0100

    print list of analyzers with -h guru (#3495)
    fixes #3494

--- opengrok-indexer/src/main/java/org/opengrok/indexer/analysis/document/MandocAnalyzerFactory.java
+++ opengrok-indexer/src/main/java/org/opengrok/indexer/analysis/document/MandocAnalyzerFactory.java
@@ -18,7 +18,7 @@
  */
 
 /*
- * Copyright (c) 2007, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2007, 2021, Oracle and/or its affiliates. All rights reserved.
  * Copyright (c) 2017, Chris Fraire <cfraire@me.com>.
  */
 package org.opengrok.indexer.analysis.document;
@@ -32,7 +32,7 @@ import org.opengrok.indexer.configuration.RuntimeEnvironment;
 
 public class MandocAnalyzerFactory extends FileAnalyzerFactory {
 
-    private static final String NAME = "Mandoc";
+    private static final String NAME = "Manual pages";
 
     public static final Matcher MATCHER = new Matcher() {
         @Override
