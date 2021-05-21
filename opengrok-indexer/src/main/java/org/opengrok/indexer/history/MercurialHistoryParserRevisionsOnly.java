8d086e530d51fb9d54fa4429d6b18683286e5382
Vladimir Kotal <vlada@devnull.cz>
2021-05-21 14:22:11 +0200

    remove unused import

--- opengrok-indexer/src/main/java/org/opengrok/indexer/history/MercurialHistoryParserRevisionsOnly.java
+++ opengrok-indexer/src/main/java/org/opengrok/indexer/history/MercurialHistoryParserRevisionsOnly.java
@@ -29,7 +29,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import java.util.function.Consumer;
 
 class MercurialHistoryParserRevisionsOnly implements Executor.StreamHandler {
