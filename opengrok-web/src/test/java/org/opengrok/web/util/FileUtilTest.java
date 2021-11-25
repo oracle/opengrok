ff54fbab385a1b782c02d2b2fd5b0502b4b52ba1
Vladimir Kotal <vlada@kotalovi.cz>
2021-11-25 14:57:12 +0100

    fix tests

--- opengrok-web/src/test/java/org/opengrok/web/util/FileUtilTest.java
+++ opengrok-web/src/test/java/org/opengrok/web/util/FileUtilTest.java
@@ -18,31 +18,54 @@
  */
 
 /*
+ * Copyright (c) 2021, Oracle and/or its affiliates. All rights reserved.
  * Copyright (c) 2020, Chris Fraire <cfraire@me.com>.
  */
 package org.opengrok.web.util;
 
 import org.junit.jupiter.api.Test;
+import org.opengrok.indexer.configuration.RuntimeEnvironment;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.UUID;
 
 /**
  * Represents a container for tests of {@link FileUtil}.
  */
-public class FileUtilTest {
+class FileUtilTest {
 
     @Test
-    public void shouldThrowOnNullArgument() {
+    void shouldThrowOnNullArgument() {
         assertThrows(NoPathParameterException.class, () -> FileUtil.toFile(null),
                 "toFile(null)");
     }
 
     @Test
-    public void shouldThrowOnMissingFile() {
-        assertThrows(FileNotFoundException.class, () -> FileUtil.toFile(
-                UUID.randomUUID().toString()), "toFile(randomUUID)");
+    void shouldThrowOnInvalidFile() {
+        String rndPath = UUID.randomUUID().toString();
+        assertThrows(InvalidPathException.class, () -> FileUtil.toFile(rndPath),
+                "toFile(randomUUID)");
+    }
+
+    @Test
+    void shouldThrowOnMissingFile() throws IOException {
+        RuntimeEnvironment env = RuntimeEnvironment.getInstance();
+        String origRoot = env.getSourceRootPath();
+        Path dir = Files.createTempDirectory("shouldThrowOnMissingFile");
+        dir.toFile().deleteOnExit();
+        env.setSourceRoot(dir.toString());
+        String filePath = Paths.get(env.getSourceRootPath(), "nonexistent").toString();
+        assertTrue(env.getSourceRootFile().isDirectory());
+        assertThrows(FileNotFoundException.class, () -> FileUtil.toFile(filePath),
+                "toFile(nonexistent)");
+        env.setSourceRoot(origRoot);
     }
 }
