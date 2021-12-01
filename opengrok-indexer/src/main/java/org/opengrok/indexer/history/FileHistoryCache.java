55bf8c96b28618182a97ad23fe48dade7d6752cd
Vladimir Kotal <vlada@kotalovi.cz>
2021-12-01 11:17:22 +0100

    fix wording

--- opengrok-indexer/src/main/java/org/opengrok/indexer/history/FileHistoryCache.java
+++ opengrok-indexer/src/main/java/org/opengrok/indexer/history/FileHistoryCache.java
@@ -813,7 +813,7 @@ class FileHistoryCache implements HistoryCache {
         String revPath = getRepositoryCachedRevPath(repository);
         if (revPath != null) {
             // remove the file cached last revision (done separately in case
-            // it gets ever moved outside of the hierarchy)
+            // it gets ever moved outside the hierarchy)
             File cachedRevFile = new File(revPath);
             try {
                 Files.delete(cachedRevFile.toPath());
