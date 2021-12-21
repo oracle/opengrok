ad5e0345ba5fb906b8cb5df7e2abdee2d2d6078a
Vladimir Kotal <vlada@kotalovi.cz>
2021-12-21 16:50:13 +0100

    synchronize setPluginDirectory(String directory)

--- opengrok-indexer/src/main/java/org/opengrok/indexer/framework/PluginFramework.java
+++ opengrok-indexer/src/main/java/org/opengrok/indexer/framework/PluginFramework.java
@@ -108,7 +108,7 @@ public abstract class PluginFramework<PluginType> {
      *
      * @param directory the directory path
      */
-    public void setPluginDirectory(String directory) {
+    public synchronized void setPluginDirectory(String directory) {
         setPluginDirectory(directory != null ? new File(directory) : null);
     }
 
