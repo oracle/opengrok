9502e0d4a1a3b222fa5f9b85839890f1811cc6e3
Vladimír Kotal <vlada@kotalovi.cz>
2022-07-15 15:26:13 +0200

    remove unused imports

--- opengrok-indexer/src/test/java/org/opengrok/indexer/web/UtilTest.java
+++ opengrok-indexer/src/test/java/org/opengrok/indexer/web/UtilTest.java
@@ -31,8 +31,6 @@ import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
