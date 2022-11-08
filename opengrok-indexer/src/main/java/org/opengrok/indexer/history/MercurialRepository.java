20147cf98454d5da325a978f6c7962c1c6e9a61a
Vladimír Kotal <vlada@kotalovi.cz>
2022-11-08 16:33:04 +0100

    do not log warning if the command actually worked

--- opengrok-indexer/src/main/java/org/opengrok/indexer/history/MercurialRepository.java
+++ opengrok-indexer/src/main/java/org/opengrok/indexer/history/MercurialRepository.java
@@ -540,8 +540,10 @@ public class MercurialRepository extends RepositoryWithHistoryTraversal {
     private static boolean isHgWorking() {
         String repoCommand = getCommand(MercurialRepository.class, CMD_PROPERTY_KEY, CMD_FALLBACK);
         boolean works = checkCmd(repoCommand);
-        LOGGER.log(Level.WARNING, "Command ''{0}'' does not work. " +
-                "Some operations with Mercurial repositories will fail as a result.", repoCommand);
+        if (!works) {
+            LOGGER.log(Level.WARNING, "Command ''{0}'' does not work. " +
+                    "Some operations with Mercurial repositories will fail as a result.", repoCommand);
+        }
         return works;
     }
 
