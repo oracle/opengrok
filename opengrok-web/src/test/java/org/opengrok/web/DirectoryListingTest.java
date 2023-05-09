40a82f0244e926f6e62b49e9c1110dc7810e7171
Vladimir Kotal <vlada@kotalovi.cz>
2023-05-09 21:50:36 +0200

    remove unused import

--- opengrok-web/src/test/java/org/opengrok/web/DirectoryListingTest.java
+++ opengrok-web/src/test/java/org/opengrok/web/DirectoryListingTest.java
@@ -37,7 +37,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
