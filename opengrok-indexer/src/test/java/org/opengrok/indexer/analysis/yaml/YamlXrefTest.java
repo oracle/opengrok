c3b1ae910f3855a57fe890f9584ca7fa5d5097c3
Gino Augustine <ginoaugustine@gmail.com>
2023-09-21 20:11:29 +0530

    Update opengrok-indexer/src/test/java/org/opengrok/indexer/analysis/yaml/YamlXrefTest.java
    Co-authored-by: Vladimir Kotal <vlada@kotalovi.cz>

--- opengrok-indexer/src/test/java/org/opengrok/indexer/analysis/yaml/YamlXrefTest.java
+++ opengrok-indexer/src/test/java/org/opengrok/indexer/analysis/yaml/YamlXrefTest.java
@@ -18,7 +18,7 @@
  */
 
 /*
- * Copyright (c) 2012, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2023, Oracle and/or its affiliates.
  * Portions Copyright (c) 2023, Gino Augustine <gino.augustine@oracle.com>.
  */
 package org.opengrok.indexer.analysis.yaml;
