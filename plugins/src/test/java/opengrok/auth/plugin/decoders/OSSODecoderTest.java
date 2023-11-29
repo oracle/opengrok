08f88303bd81eca659ea8ed65e45e06c8410d79f
Vladimir Kotal <vlada@kotalovi.cz>
2023-11-29 11:36:10 +0100

    bump year

--- plugins/src/test/java/opengrok/auth/plugin/decoders/OSSODecoderTest.java
+++ plugins/src/test/java/opengrok/auth/plugin/decoders/OSSODecoderTest.java
@@ -18,7 +18,7 @@
  */
 
 /*
- * Copyright (c) 2017, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, 2023, Oracle and/or its affiliates. All rights reserved.
  */
 package opengrok.auth.plugin.decoders;
 
