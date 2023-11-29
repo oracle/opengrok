ad46a73dbb25306cdbad53f31141723c3746f888
Vladimir Kotal <vladimir.kotal@oracle.com>
2023-11-29 17:25:27 +0100

    do not call getSCCSFile() twice (#4489)

--- opengrok-indexer/src/main/java/org/opengrok/indexer/history/SCCSHistoryParser.java
+++ opengrok-indexer/src/main/java/org/opengrok/indexer/history/SCCSHistoryParser.java
@@ -18,7 +18,7 @@
  */
 
 /*
- * Copyright (c) 2007, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2007, 2023, Oracle and/or its affiliates. All rights reserved.
  */
 package org.opengrok.indexer.history;
 
@@ -82,7 +82,7 @@ final class SCCSHistoryParser {
             return null;
         }
 
-        in = new BufferedReader(new FileReader(getSCCSFile(file)));
+        in = new BufferedReader(new FileReader(f));
         pass = sep = false;
         passRecord = true;
         active = true;
