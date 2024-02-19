12f29f7d7e82ba89740540f0eaf70c9b1c5941fc
Vladimir Kotal <vlada@kotalovi.cz>
2024-02-19 11:31:56 +0100

    bump year

--- opengrok-indexer/src/main/java/org/opengrok/indexer/history/AbstractCache.java
+++ opengrok-indexer/src/main/java/org/opengrok/indexer/history/AbstractCache.java
@@ -18,7 +18,7 @@
  */
 
 /*
- * Copyright (c) 2005, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2005, 2024, Oracle and/or its affiliates. All rights reserved.
  */
 package org.opengrok.indexer.history;
 
