2583501889bc2288a047fb1b9eda37600609ffda
Vladimir Kotal <vlada@kotalovi.cz>
2024-08-12 15:34:05 +0200

    bump year

--- opengrok-indexer/src/main/java/org/opengrok/indexer/framework/PluginFramework.java
+++ opengrok-indexer/src/main/java/org/opengrok/indexer/framework/PluginFramework.java
@@ -18,7 +18,7 @@
  */
 
 /*
- * Copyright (c) 2019, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019, 2024, Oracle and/or its affiliates. All rights reserved.
  */
 package org.opengrok.indexer.framework;
 
