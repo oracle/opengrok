882d99be15e64c6e2ebdd9f98211257033cce8d1
Vladimir Kotal <vlada@kotalovi.cz>
2024-09-11 11:09:55 +0200

    apply black

--- docker/start.py
+++ docker/start.py
@@ -456,7 +456,11 @@ def create_bare_config(logger, use_projects, extra_indexer_options=None):
         indexer_options.extend(extra_indexer_options)
     if use_projects:
         indexer_options.append("-P")
-    indexer = Indexer(indexer_options, java_opts=indexer_java_opts, jar=OPENGROK_JAR, logger=logger, doprint=True)
+        indexer_options,
+        java_opts=indexer_java_opts,                                            
+        jar=OPENGROK_JAR,                                                       
+        logger=logger,                                                          
+        doprint=True,
     indexer.execute()
     ret = indexer.getretcode()
     if ret != SUCCESS_EXITVAL:
