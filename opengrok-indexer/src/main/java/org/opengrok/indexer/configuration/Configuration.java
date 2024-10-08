6fe405085f982e8b11d93d50a8101dae9ce58fa2
Vladimir Kotal <vlada@kotalovi.cz>
2024-10-08 09:48:25 +0200

    bump year

--- opengrok-indexer/src/main/java/org/opengrok/indexer/configuration/Configuration.java
+++ opengrok-indexer/src/main/java/org/opengrok/indexer/configuration/Configuration.java
@@ -18,7 +18,7 @@
  */
 
 /*
- * Copyright (c) 2007, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2007, 2024, Oracle and/or its affiliates. All rights reserved.
  * Portions Copyright (c) 2017, 2020, Chris Fraire <cfraire@me.com>.
  * Portions Copyright (c) 2020, Aleksandr Kirillov <alexkirillovsamara@gmail.com>.
  */
