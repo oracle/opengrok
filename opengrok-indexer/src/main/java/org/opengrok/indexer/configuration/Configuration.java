bb120907f2aeb32a55ed6d599127482998131c4a
Vladimir Kotal <vlada@kotalovi.cz>
2024-10-08 09:43:15 +0200

    quote path in log statement

--- opengrok-indexer/src/main/java/org/opengrok/indexer/configuration/Configuration.java
+++ opengrok-indexer/src/main/java/org/opengrok/indexer/configuration/Configuration.java
@@ -1510,7 +1510,7 @@ public final class Configuration {
     }
 
     public static Configuration read(File file) throws IOException {
-        LOGGER.log(Level.INFO, "Reading configuration from {0}", file.getCanonicalPath());
+        LOGGER.log(Level.INFO, "Reading configuration from ''{0}''", file.getCanonicalPath());
         try (FileInputStream in = new FileInputStream(file)) {
             return decodeObject(in);
         }
