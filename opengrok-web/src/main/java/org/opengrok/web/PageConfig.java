49a599b91fa63a3623226acfa58d2703b44e15ee
Ed Baunton <edbaunton@gmail.com>
2025-01-27 12:42:43 +0000

    Add option to disable immediate redirect on singular results
    Per #3582 add an option to disable the redirection behaviour when there is only a single file result.
    
    Signed-off-by: Ed Baunton <edbaunton@gmail.com>

--- opengrok-web/src/main/java/org/opengrok/web/PageConfig.java
+++ opengrok-web/src/main/java/org/opengrok/web/PageConfig.java
@@ -1520,7 +1520,12 @@ public class PageConfig {
      * @return a search helper.
      */
     public SearchHelper prepareInternalSearch(SortOrder sortOrder) {
+        boolean isRedirectOnSingleSearchResult = getEnv().isRedirectOnSingleSearchResult();
         String xrValue = req.getParameter(QueryParameters.NO_REDIRECT_PARAM);
+        if (xrValue != null && !xrValue.isEmpty()) {
+            isRedirectOnSingleSearchResult = false;
+        }
+
         return new SearchHelper.Builder(getDataRoot(), new File(getSourceRootPath()), getEftarReader(),
                 getQueryBuilder(), req.getContextPath())
                 .start(getStartIndex())
@@ -1528,7 +1533,7 @@ public class PageConfig {
                 .maxItems(getMaxItems())
                 .crossRefSearch(getPrefix() == Prefix.SEARCH_R)
                 .guiSearch(getPrefix() == Prefix.SEARCH_R || getPrefix() == Prefix.SEARCH_P)
-                .noRedirect(xrValue != null && !xrValue.isEmpty())
+                .noRedirect(!isRedirectOnSingleSearchResult)
                 .build();
     }
 
